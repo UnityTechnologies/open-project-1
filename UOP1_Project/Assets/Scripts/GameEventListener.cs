e49b0d5ffa90862698b98a3176e24f16815a3ccf
Unknown <amel.negra@unity3d.com>
2020-08-20 18:36:11 +0100

    Adding events and listeners for scene loading

---
+++ UOP1_Project/Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        //Check if the event exists to avoid errors
+        if (Event == null)
+        {
+            return;
+        }
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        if (Event == null)
+        {
+            return;
+        }
+        Event.UnregisterListener(this); 
+    }
+
+    public void OnEventRaised()
+    {
+        if (Response == null)
+        {
+            return;
+        }
+        Response.Invoke(); 
+    }
+}
