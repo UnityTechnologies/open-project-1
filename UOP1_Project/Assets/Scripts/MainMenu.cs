7cac91c16d1b242bd33b3f5d4b23a625f28e2d4b
Unknown <amel.negra@unity3d.com>
2020-08-20 18:36:27 +0100

    Main menu and loading bar scripts

---
+++ UOP1_Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MainMenu : MonoBehaviour
+{
+    //Check the bool to load MM from ScenesLoaders on start
+    public bool LoadOnStart;
+
+    public GameEvent onGameStart;
+
+    public void Start()
+    {
+        if (LoadOnStart)
+        {
+            onGameStart.Raise();
+        }
+    }
+
+    public void Update()
+    {
+        //We can load the main menu by pressing space bar in the Scenes Loader scene
+        //Just for test purpose
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            onGameStart.Raise();
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit dude!");
+    }
+
+}
