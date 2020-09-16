755d72392f521402ef65ffe4040925587780e76f
Ciro Continisio <ciro@unity3d.com>
2020-09-17 00:48:45 +0200

    Toon shader improvments
    - Updated URP from 7.3.0 to 7.5.1
    - Moved to most recent 19.4
    - New custom lighting shader (Toon 2), not yet toonified

--- UOP1_Project/Assets/Shaders/CustomLighting.hlsl
+++ UOP1_Project/Assets/Shaders/CustomLighting.hlsl
@@ -9,30 +9,22 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
     DistanceAtten = 1;
     ShadowAtten = 1;
 #else
-	#if SHADOWS_SCREEN
-		float4 clipPos = TransformWorldToHClip(WorldPos);
-		float4 shadowCoord = ComputeScreenPos(clipPos);
-	#else
-		float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-	#endif
+	float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+
     Light mainLight = GetMainLight(shadowCoord);
     Direction = mainLight.direction;
     Color = mainLight.color;
     DistanceAtten = mainLight.distanceAttenuation;
 
 	#if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
-            ShadowAtten = 1.0h;
-        #endif
- 
-        #if SHADOWS_SCREEN
-            ShadowAtten = SampleScreenSpaceShadowmap(shadowCoord);
-        #else
-            ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-            float shadowStrength = GetMainLightShadowStrength();
-            ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
-            sampler_MainLightShadowmapTexture),
-            shadowSamplingData, shadowStrength, false);
-        #endif
+		ShadowAtten = 1.0h;
+	#endif
+
+	ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+	float shadowStrength = GetMainLightShadowStrength();
+	ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
+	sampler_MainLightShadowmapTexture),
+	shadowSamplingData, shadowStrength, false);
 #endif
 }
 
