29504f6fa5b3289a5cbf22ebcc3a63b14993fc3c
Emre Bugday <47198270+EmreB99@users.noreply.github.com>
2020-09-30 20:39:21 +0300

    Created the Spawn System
    In addition of spawning the character on scene changes + with a publicly callable SpawnCharacter method for runtime spawnings, I also implemented an event system which gets invoked after a character is spawned in scene. The event takes the GameObject that was spawned as a parameter for furthermore runtime checks. This is an utility that I use on my own personal projects to check from my game manager which player is spawned, do the spawned player has any specific components, tags etc than act accordingly.

---
+++ UOP1_Project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[Serializable]
+public struct SpawnData
+{
+    //I couldn't decide on whether to use build index or scene name. Please let me know if any change is needed either from GitHub or from emreb25@outlook.com
+    [Tooltip("If game loads THIS scene during runtime, parameters you set below will be used for spawning.")]
+    public string sceneName;
+
+    [Tooltip("The spawn position of the character.")]
+    public Vector3 spawnPosition;
+
+    [Tooltip("The spawn rotation of the character.")]
+    public Vector3 spawnRotation;
+
+    //In case we change the character prefab for different scenes for same various reasons.
+    [Tooltip("The character to spawn.")]
+    public GameObject characterToSpawn;
+}
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+
+#pragma warning disable 0649
+
+    /// <summary>
+    /// Should be constructed/edited within the Editor, not in runtime.
+    /// </summary>
+    [SerializeField]
+    private SpawnData[] spawnData;
+
+#pragma warning restore 0649
+
+    public delegate void CharacterSpawnHandler(GameObject spawnedObject);
+    public event CharacterSpawnHandler CharacterSpawned;
+
+    #region Initialization
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnEnable() => SceneManager.sceneLoaded += OnSceneLoad;
+
+    private void OnDisable() => SceneManager.sceneLoaded -= OnSceneLoad;
+
+    #endregion Initialization
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        foreach (var data in spawnData)
+        {
+            if (scene.name == data.sceneName)
+                SpawnCharacter(data.characterToSpawn, data.spawnPosition, data.spawnRotation);
+        }
+    }
+
+    /// <summary>
+    /// Spawns the character in scene.
+    /// </summary>
+    /// <param name="prefabToSpawn">The character's prefab to spawn.</param>
+    /// <param name="spawnPos">Spawn position of the character.</param>
+    /// <param name="spawnRot">Spawn rotation of the character.</param>
+    public void SpawnCharacter(GameObject prefabToSpawn, Vector3 spawnPos, Vector3 spawnRot)
+    {
+        Instantiate(prefabToSpawn, spawnPos, Quaternion.Euler(spawnRot));
+        CharacterSpawned?.Invoke(prefabToSpawn);
+    }
+
+    /// <summary>
+    /// Spawns the character in scene.
+    /// </summary>
+    /// <param name="prefabToSpawn">The character's prefab/GameObject to spawn.</param>
+    /// <param name="spawnPos">Spawn position of the character.</param>
+    public void SpawnCharacter(GameObject prefabToSpawn, Vector3 spawnPos)
+    {
+        Instantiate(prefabToSpawn, spawnPos, Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f)));
+        CharacterSpawned?.Invoke(prefabToSpawn);
+    }
+}
