41ea5ff9e7e8450c276012af1daba7a9edff1bc1
Ayhan Sakarya <acimaz90@gmail.com>
2020-10-02 01:40:59 +0200

    - Implemented General tab - Language setting - Implemented Graphics tab - Graphics Presets, Resolution (all supported resolutions on users device), fullscreen mode settings - Implemented Advanced Graphics - Anisotropic filtering, Anti-Aliasing, Shadow Distance, Shadow Quality settings - Implemented Audio tab - Music Volume, Sfx Volume settings

--- UOP1_Project/Assets/Scripts/Systems/SettingsSystem.cs
+++ UOP1_Project/Assets/Scripts/Systems/SettingsSystem.cs
@@ -1,30 +1,128 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class SettingsSystem : MonoBehaviour
 {
     [SerializeField] TMP_Dropdown resolutionsDropdown;
-    
-    public bool FullScreen { get; private set; }
+    [SerializeField] TMP_Dropdown languageDropdown;
+    [SerializeField] TMP_Dropdown shadowQualityDropdown;
+    [SerializeField] TMP_Dropdown anisotropicFilteringDropdown;
+    [SerializeField] TMP_Dropdown qualityPresetsDropdown;
+    [SerializeField] Slider antiAliasingSlider;
+    [SerializeField] Slider shadowDistanceSlider;
+    [SerializeField] Slider musicVolumeSlider;
+    [SerializeField] Slider sfxVolumeSlider;
+    [SerializeField] TextMeshProUGUI antiAliasingText;
+    [SerializeField] TextMeshProUGUI shadowDistanceText;
 
-    void Awake()
+    public bool FullScreen { get; private set; }
+    public float MusicVolume { get; private set; }
+    public float SfxVolume { get; private set; }
+    public LanguageSetting Language { get; private set; }
+    public AdvancedGraphicsSettings AdvancedGraphics { get; private set; }
+
+    AdvancedGraphicsSettings previousSettings;
+
+    public enum LanguageSetting
     {
-        EnsureGameObjectIntegrity();
-        resolutionsDropdown.AddOptions(GetDropdownResolutionData());
+        English,
+        German,
+        //TODO: which languages are going to be supported?
     }
 
-    void EnsureGameObjectIntegrity()
+    public struct AdvancedGraphicsSettings
     {
-        if (resolutionsDropdown == null)
+        public ShadowQuality shadowQuality;
+        public AnisotropicFiltering anisotropicFiltering;
+        public int antiAliasing;
+        public float shadowDistance;
+        public bool custom;
+        public int qualityLevel;
+    }
+
+    void Start()
+    {
+        //TODO: Load previous settings data via save/load interface
+        resolutionsDropdown.AddOptions(GetResolutionsDropdownData());
+        languageDropdown.AddOptions(GetDropdownData(Enum.GetNames(typeof(LanguageSetting))));
+        qualityPresetsDropdown.AddOptions(GetDropdownData(QualitySettings.names));
+        foreach (string s in QualitySettings.names)
         {
-            resolutionsDropdown = transform.Find("Resolutions Dropdown").GetComponent<TMP_Dropdown>();
+            Debug.Log(s);
+        }
+        anisotropicFilteringDropdown.AddOptions(GetDropdownData(Enum.GetNames(typeof(AnisotropicFiltering))));
+        shadowQualityDropdown.AddOptions(GetDropdownData(Enum.GetNames(typeof(ShadowQuality))));
+        LoadCurrentSettings();
+    }
+
+    void LoadCurrentSettings()
+    {
+        int resolutionIndex = 0;
+        //TODO: load previous resolution setting. If not existing find current resolution index
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            if (Screen.currentResolution.ToString() == Screen.resolutions[i].ToString())
+            {
+                resolutionIndex = i;
+            }
+        }
+
+        resolutionsDropdown.SetValueWithoutNotify(resolutionIndex);
+        //TODO: load previous quality level. If custom, set qualityPresetsDropdown to custom. Option "custom" is added in GetQualityPresetsDropdownData()
+        AdvancedGraphics = new AdvancedGraphicsSettings()
+        {
+            anisotropicFiltering = QualitySettings.anisotropicFiltering,
+            antiAliasing = QualitySettings.antiAliasing,
+            shadowDistance = QualitySettings.shadowDistance,
+            shadowQuality = QualitySettings.shadows,
+            custom = false,
+            qualityLevel = qualityPresetsDropdown.options.Count-1
+        };
+        previousSettings = AdvancedGraphics;
+        qualityPresetsDropdown.SetValueWithoutNotify(AdvancedGraphics.custom ? qualityPresetsDropdown.options.Count-1 : QualitySettings.GetQualityLevel());
+
+        UpdateAdvancedGraphicsUI();
+
+        //TODO: load previous language setting
+        Language = LanguageSetting.English;
+        languageDropdown.SetValueWithoutNotify((int) Language);
+        //TODO: load previous fullscreen setting
+        Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        //TODO: load previous music volume setting
+        MusicVolume = 0.5f;
+        musicVolumeSlider.SetValueWithoutNotify(MusicVolume);
+        //TODO: load previous sfx volume setting
+        SfxVolume = 0.5f;
+        sfxVolumeSlider.SetValueWithoutNotify(SfxVolume);
+    }
+
+    void SelectGraphicsPreset(int level)
+    {
+        QualitySettings.SetQualityLevel(0, true);
+        UpdateAdvancedGraphicsUI();
+    }
+
+    void UpdateAdvancedGraphicsUI()
+    {
+        anisotropicFilteringDropdown.SetValueWithoutNotify((int) AdvancedGraphics.anisotropicFiltering);
+        antiAliasingSlider.SetValueWithoutNotify(AdvancedGraphics.antiAliasing);
+        shadowDistanceSlider.SetValueWithoutNotify(AdvancedGraphics.shadowDistance);
+        shadowQualityDropdown.SetValueWithoutNotify((int) AdvancedGraphics.shadowQuality);
+        shadowDistanceText.text = AdvancedGraphics.shadowDistance.ToString();
+        antiAliasingText.text = AdvancedGraphics.antiAliasing.ToString();
+        if (!AdvancedGraphics.custom)
+        {
+            qualityPresetsDropdown.SetValueWithoutNotify(AdvancedGraphics.qualityLevel);
         }
     }
-    
-    List<TMP_Dropdown.OptionData> GetDropdownResolutionData()
+
+    List<TMP_Dropdown.OptionData> GetResolutionsDropdownData()
     {
         List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
         for (int i = 0; i < Screen.resolutions.Length; i++)
@@ -35,6 +133,29 @@ public class SettingsSystem : MonoBehaviour
         return options;
     }
 
+    List<TMP_Dropdown.OptionData> GetDropdownData(string[] optionNames)
+    {
+        List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+        foreach (string option in optionNames)
+        {
+            options.Add(new TMP_Dropdown.OptionData(option));
+        }
+
+        return options;
+    }
+
+    #region GENERAL SETTINGS
+
+    public void OnChangeLanguage(int languageIndex)
+    {
+        Language = (LanguageSetting) languageIndex;
+        Debug.Log("Language set to: " + Language);
+    }
+
+    #endregion
+
+    #region GRAPHICS SETTINGS
+
     public void OnChangeFullscreen(bool fullScreen)
     {
         Screen.fullScreenMode = fullScreen ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed;
@@ -44,5 +165,108 @@ public class SettingsSystem : MonoBehaviour
     {
         Resolution newResolution = Screen.resolutions[resolutionIndex];
         Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreenMode);
+    }
+
+    public void OnChangeAnisotropicFiltering(int anisoLevel)
+    {
+        AdvancedGraphicsSettings newSettings = AdvancedGraphics;
+        newSettings.anisotropicFiltering = (AnisotropicFiltering) anisoLevel;
+        newSettings.custom = true;
+        AdvancedGraphics = newSettings;
+        UpdateAdvancedGraphicsUI();
+    }
+
+    public void OnChangeAntialiasing(float value)
+    {
+        AdvancedGraphicsSettings newSettings = AdvancedGraphics;
+        newSettings.antiAliasing = (int)value;
+        newSettings.custom = true;
+        AdvancedGraphics = newSettings;
+        UpdateAdvancedGraphicsUI();
+    }
+
+    public void OnChangeShadowDistance(float shadowDistanceValue)
+    {
+        //TODO: configure min max value in slider
+        AdvancedGraphicsSettings newSettings = AdvancedGraphics;
+        newSettings.shadowDistance = shadowDistanceValue;
+        newSettings.custom = true;
+        AdvancedGraphics = newSettings;
+        UpdateAdvancedGraphicsUI();
+    }
+
+    public void OnChangeShadowQuality(int level)
+    {
+        AdvancedGraphicsSettings newSettings = AdvancedGraphics;
+        newSettings.shadowQuality = (ShadowQuality) level;
+        newSettings.custom = true;
+        AdvancedGraphics = newSettings;
+        UpdateAdvancedGraphicsUI();
+    }
+
+    public void OnChangeQualityPreset(int level)
+    {
+        QualitySettings.SetQualityLevel(level, true);
+        Debug.Log("Current quality: " + QualitySettings.names[QualitySettings.GetQualityLevel()]);
+        Debug.Log("Antialiasing: " + QualitySettings.antiAliasing);
+        Debug.Log("Shadow Distance: " + QualitySettings.shadowDistance);
+        AdvancedGraphicsSettings newSettings = AdvancedGraphics;
+        newSettings.anisotropicFiltering = QualitySettings.anisotropicFiltering;
+        newSettings.antiAliasing = QualitySettings.antiAliasing;
+        newSettings.shadowDistance = QualitySettings.shadowDistance;
+        newSettings.shadowQuality = QualitySettings.shadows;
+        newSettings.qualityLevel = level;
+        newSettings.custom = false;
+        AdvancedGraphics = newSettings;
+        UpdateAdvancedGraphicsUI();
+    }
+
+    #endregion
+
+    #region AUDIO SETTINGS
+
+    //TODO: clamp volume to [0, 1] or [0, 100]? Change Slider min max value in editor depending on use case
+    public void OnChangeMusicVolume(float volume)
+    {
+        MusicVolume = Mathf.Clamp(volume, 0.0f, 1.0f);
+    }
+
+    //TODO: clamp volume to [0, 1] or [0, 100]? Change Slider min max value in editor depending on use case
+    public void OnChangeSfxVolume(float volume)
+    {
+        SfxVolume = Mathf.Clamp(volume, 0.0f, 1.0f);
+    }
+
+    #endregion
+
+    public void OnSaveGraphicsSettings()
+    {
+        if (AdvancedGraphics.custom)
+        {
+            QualitySettings.anisotropicFiltering = AdvancedGraphics.anisotropicFiltering;
+            QualitySettings.antiAliasing = AdvancedGraphics.antiAliasing;
+            QualitySettings.shadowDistance = AdvancedGraphics.shadowDistance;
+            QualitySettings.shadows = AdvancedGraphics.shadowQuality;
+        }
+        //No need to save non custom settings as these are applied straight away in OnChangeQualityPreset because there is no way of iterating through unitys quality presets atm
+        previousSettings = AdvancedGraphics;
+        Debug.Log("Antialiasing: " + QualitySettings.antiAliasing);
+    }
+
+    public void OnCancelGraphicsSettings()
+    {
+        AdvancedGraphics = previousSettings;
+        if (AdvancedGraphics.custom)
+        {
+            QualitySettings.anisotropicFiltering = AdvancedGraphics.anisotropicFiltering;
+            QualitySettings.antiAliasing = AdvancedGraphics.antiAliasing;
+            QualitySettings.shadowDistance = AdvancedGraphics.shadowDistance;
+            QualitySettings.shadows = AdvancedGraphics.shadowQuality;
+        }
+        else
+        {
+            QualitySettings.SetQualityLevel(AdvancedGraphics.qualityLevel);
+        }
+        UpdateAdvancedGraphicsUI();
     }
 }
