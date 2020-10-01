8418894e9521352bc82aa201614be757fcaa9da1
Ayhan Sakarya <acimaz90@gmail.com>
2020-10-01 17:20:46 +0200

    - Imported Textmesh Pro
    - Created Settings System scene for testing
    
    - Added a few basic UI elements (not complete yet)
    
    - Started to add features for System Settings: Fullscreen mode, Resolution Dropdown
    
    - Added Settings System scene to build settings
    
    - Project and graphic setting changes automatically made by unity editor because i'm using mac (shouldn't affect windows)

---
+++ UOP1_Project/Assets/Scripts/Systems/SettingsSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class SettingsSystem : MonoBehaviour
+{
+    [SerializeField] TMP_Dropdown resolutionsDropdown;
+    
+    public bool FullScreen { get; private set; }
+
+    void Awake()
+    {
+        EnsureGameObjectIntegrity();
+        resolutionsDropdown.AddOptions(GetDropdownResolutionData());
+    }
+
+    void EnsureGameObjectIntegrity()
+    {
+        if (resolutionsDropdown == null)
+        {
+            resolutionsDropdown = transform.Find("Resolutions Dropdown").GetComponent<TMP_Dropdown>();
+        }
+    }
+    
+    List<TMP_Dropdown.OptionData> GetDropdownResolutionData()
+    {
+        List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            options.Add(new TMP_Dropdown.OptionData(Screen.resolutions[i].ToString()));
+        }
+
+        return options;
+    }
+
+    public void OnChangeFullscreen(bool fullScreen)
+    {
+        Screen.fullScreenMode = fullScreen ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed;
+    }
+
+    public void OnChangeResolution(int resolutionIndex)
+    {
+        Resolution newResolution = Screen.resolutions[resolutionIndex];
+        Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreenMode);
+    }
+}
