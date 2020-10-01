21443108cce54e239708fe6355d2785f66a25f94
Ayhan Sakarya <acimaz90@gmail.com>
2020-10-02 01:40:59 +0200

    - Implemented General tab - Language setting - Implemented Graphics tab - Graphics Presets, Resolution (all supported resolutions on users device), fullscreen mode settings - Implemented Advanced Graphics - Anisotropic filtering, Anti-Aliasing, Shadow Distance, Shadow Quality settings - Implemented Audio tab - Music Volume, Sfx Volume settings

---
+++ UOP1_Project/Assets/Scripts/Systems/SettingsUIController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUIController : MonoBehaviour
+{
+    [SerializeField] GameObject generalSettings, graphicsSettings, audioSettings;
+    [SerializeField] Button generalSettingsButton, graphicsSettingsButton, audioSettingsButton, saveGraphicsSettingsButton, cancelGraphicsSettingsButton;
+
+    public enum SettingsType
+    {
+        General,
+        Graphics,
+        Audio
+    }
+
+    public void OpenGeneralSettings()
+    {
+        OpenSetting(SettingsType.General);
+    }
+
+    public void OpenGraphicsSettings()
+    {
+        OpenSetting(SettingsType.Graphics);
+    }
+
+    public void OpenAudioSettings()
+    {
+        OpenSetting(SettingsType.Audio);
+    }
+
+    public void OnSaveGraphicsSettings()
+    {
+        saveGraphicsSettingsButton.interactable = false;
+    }
+
+    void OpenSetting(SettingsType settingType)
+    {
+        generalSettings.SetActive(settingType == SettingsType.General);
+        graphicsSettings.SetActive((settingType == SettingsType.Graphics));
+        audioSettings.SetActive(settingType == SettingsType.Audio);
+        
+        generalSettingsButton.interactable = settingType != SettingsType.General;
+        graphicsSettingsButton.interactable = settingType != SettingsType.Graphics;
+        audioSettingsButton.interactable = settingType != SettingsType.Audio;
+    }
+}
