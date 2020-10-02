5e4713f3d1c1859f8819513d1b611ce7ca1dc574
Dan <d.szabo87@gmail.com>
2020-10-02 15:52:21 +1000

    Add Dialogue Track base implementation

---
+++ UOP1_Project/Assets/Scripts/Cutscenes/CutsceneManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UOP1.Dialogue;
+
+namespace UOP1.Cutscene
+{
+    public class CutsceneManager : MonoBehaviour
+    {
+#pragma warning disable 649
+        [SerializeField] private TimelineAsset m_timeline;
+#pragma warning restore 649
+
+        private PlayableDirector m_director;
+        
+        private void Awake()
+        {
+            // TODO this used to create an instance of the PseudoDialogueSystem.
+            // Normally this would be created by the Game Manager.
+            PseudoDialogueSystem dialogueSystem = new PseudoDialogueSystem();
+
+            m_director = GetComponent<PlayableDirector>();
+        }
+
+        private void Start()
+        {
+            PlayCutscene(m_timeline);
+        }
+
+        public void PlayCutscene(TimelineAsset cutscene)
+        {
+            // TODO block player input, keeping dialogue input enabled
+            
+            m_director.playableAsset = cutscene;
+            m_director.Evaluate();
+            m_director.Play();
+            m_director.stopped += OnCutsceneCompleted;
+        }
+
+        private void OnCutsceneCompleted(PlayableDirector director)
+        {
+            director.stopped -= OnCutsceneCompleted;
+            
+            // TODO return to normal gameplay
+        }
+    }
+}
---
+++ UOP1_Project/Assets/Scripts/Cutscenes/DialogClip/DialogueClip.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace UOP1.Cutscenes
+{
+    [Serializable]
+    public class DialogueClip : PlayableAsset, ITimelineClipAsset
+    {
+        public DialogueBehaviour dialogue = new DialogueBehaviour ();
+
+        public ClipCaps clipCaps
+        {
+            get { return ClipCaps.None; }
+        }
+
+        public override Playable CreatePlayable (PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<DialogueBehaviour>.Create (graph, dialogue);
+            return playable;
+        }
+    }
+}
