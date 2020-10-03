cd2fc51b4424bec397b0ab94e744961f925d18fd
Indika Wijesooriya <indika@arimaclanka.com>
2020-10-03 08:51:58 +0530

    Added a Title Menu Scene with a Sceneloading script
    Added a new scene for the title menu, including a button as a prefab which can be modified lateron. Also added a small scrpt to handle sceneloads. This can also be used to manage the scene loading animation / fades to load the scenes.

---
+++ UOP1_Project/Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    //TODO - Use an enum to save the scenes
+
+    public void LoadScene(string scene)
+    {
+        //TODO - Loading Animation
+        SceneManager.LoadScene(scene);
+    }
+}
