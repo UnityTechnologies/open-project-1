2b6ae43ea656ef16f3b0405e2534a11e2e4f8e91
dkaloger <dkaloger@outlook.com>
2020-10-04 11:42:57 +0300

    dialogue

---
+++ UOP1_Project/Assets/Dialogue/scripts/conversation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Conversation", menuName = "ScriptableObjects/Conversation", order = 1)]
+public class conversation : ScriptableObject
+{
+    public Sentence[] lines;
+    public bool triggered_once =false;
+    private void Awake()
+    {
+        triggered_once = false;
+}
+}
