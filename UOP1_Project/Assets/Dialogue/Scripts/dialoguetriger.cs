751779a64c033b10fdc9b8802a55d047b4b7460b
dkaloger <dkaloger@outlook.com>
2020-10-04 12:19:59 +0300

    dialogue

---
+++ UOP1_Project/Assets/Dialogue/Scripts/dialoguetriger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialoguetriger : MonoBehaviour
+{
+  
+    [SerializeField] public conversation Conversation;
+ 
+   
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name == "Pig" )
+        {
+            GameObject.FindGameObjectWithTag("subtitles").GetComponent<DialogueRendererer>().StartCoroutine(GameObject.FindGameObjectWithTag("subtitles").GetComponent<DialogueRendererer>().NewChat(Conversation)); ;
+         
+        }
+
+    }
+}
