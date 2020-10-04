9bb2ddb6029772958f6cfcd34512b3794efe5610
dkaloger <dkaloger@outlook.com>
2020-10-04 12:48:24 +0300

    final

---
+++ UOP1_Project/Assets/Dialogue/scripts/Sentence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class Sentence 
+{
+    public string text;
+    public Color color;
+    public FontStyle fontstyle;
+    public Sprite Speaking;
+  [Tooltip("dont enter anything to use the sprite name")]  public string NameOveride;
+}
