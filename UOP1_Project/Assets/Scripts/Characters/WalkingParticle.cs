571cec8c1dab6000018409dc00f3a92775a7eb2e
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2020-10-05 11:49:47 +0700

    Improved code quality

--- UOP1_Project/Assets/Scripts/Characters/WalkingParticle.cs
+++ UOP1_Project/Assets/Scripts/Characters/WalkingParticle.cs
@@ -7,23 +7,27 @@ public class WalkingParticle : MonoBehaviour
 
     [Tooltip("Particle prefab to be attached to the charactor in the scene.")] public ParticleSystem walkingParticlePrefab;
 
-    private Vector3 lastPosition;
-    private Vector3 currentPosition;
+    private Vector2 lastPosition;
+    private Vector2 currentPosition;
     private ParticleSystem particleObject;
+
+    private void OnEnable()
+    {
+        lastPosition = new Vector2(transform.position.x, transform.position.z);
+        currentPosition = new Vector2(transform.position.x, transform.position.z);
+    }
 
     private void Update()
     {
-        if (lastPosition == null) lastPosition = transform.position;
-        if (currentPosition == null) currentPosition = transform.position;
-        if (particleObject == null && walkingParticlePrefab != null)
+        if (!particleObject && walkingParticlePrefab)
         {
             particleObject = Instantiate(walkingParticlePrefab, transform);
             particleObject.Stop();
         }
-        if(currentPosition != null && lastPosition != null && particleObject != null)
+        if(particleObject)
         {
-            currentPosition = transform.position;
-            if(currentPosition.x * 1000 != lastPosition.x * 1000 || currentPosition.z * 1000 != lastPosition.z * 1000)
+            currentPosition = new Vector2(transform.position.x, transform.position.z);
+            if(Vector2.Distance(currentPosition, lastPosition) > 0)
             {
                 if(!particleObject.isPlaying) particleObject.Play();
             }
