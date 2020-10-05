75b6dcccc5dc91a906008a42b2172c2e94fcf817
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2020-10-05 12:53:56 +0700

    Improved code quality

--- UOP1_Project/Assets/Scripts/Characters/WalkingParticle.cs
+++ UOP1_Project/Assets/Scripts/Characters/WalkingParticle.cs
@@ -6,6 +6,7 @@ public class WalkingParticle : MonoBehaviour
 {
 
     [Tooltip("Particle prefab to be attached to the charactor in the scene.")] public ParticleSystem walkingParticlePrefab;
+    [Tooltip("Minimum walking distance to play the particle.")] public float walkingDistance;
 
     private Vector2 lastPosition;
     private Vector2 currentPosition;
@@ -15,19 +16,19 @@ public class WalkingParticle : MonoBehaviour
     {
         lastPosition = new Vector2(transform.position.x, transform.position.z);
         currentPosition = new Vector2(transform.position.x, transform.position.z);
-    }
-
-    private void Update()
-    {
         if (!particleObject && walkingParticlePrefab)
         {
             particleObject = Instantiate(walkingParticlePrefab, transform);
             particleObject.Stop();
         }
+    }
+
+    private void Update()
+    {
         if(particleObject)
         {
             currentPosition = new Vector2(transform.position.x, transform.position.z);
-            if(Vector2.Distance(currentPosition, lastPosition) > 0)
+            if(Vector2.Distance(currentPosition, lastPosition) > walkingDistance)
             {
                 if(!particleObject.isPlaying) particleObject.Play();
             }
