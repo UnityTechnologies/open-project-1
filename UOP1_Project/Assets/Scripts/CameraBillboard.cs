99bcdc25cefd1447f4589fd01ae97227774aa2c5
Wiley <jthom330@gmail.com>
2020-10-04 20:54:29 -0400

    * Added basic fire effect * Contains 2d billboarded fire animation and particle systems for sparks and smoke.

---
+++ UOP1_Project/Assets/Scripts/CameraBillboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBillboard : MonoBehaviour
+{
+
+    private Camera mainCam;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 camForward = mainCam.transform.forward;
+        transform.forward = new Vector3(camForward.x, transform.forward.y, camForward.z);
+    }
+ }
