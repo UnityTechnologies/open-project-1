8791a107761fec133c8018b159bab6aca649dba0
OTGLLC <58696599+OTGLLC@users.noreply.github.com>
2020-10-05 06:24:23 -0500

    AnimationHandler updated
    The animation handler can playback animation using the playable graph

--- UOP1_Project/Assets/Scripts/Statemachine/Core/AnimationHandler.cs
+++ UOP1_Project/Assets/Scripts/Statemachine/Core/AnimationHandler.cs
@@ -1,30 +1,68 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.Timeline;
-using System.Security.Policy;
+using UnityEngine.Playables;
+using UnityEngine.Animations;
 
 namespace CombatStatemachine
 {
     public class AnimationHandler
     {
-        #region Properties
-        public Animator AnimatorComponent { get; private set; }
-        public AnimationHandlerData Data { get; private set; }
+        #region Fields
+        private Animator m_animComponent;
+        private AnimationHandlerData m_data;
+        private AnimationClip m_currentClip;
+        private PlayableGraph m_playableGraph;
+        private AnimationPlayableOutput m_playableOutput;
+        private AnimationClipPlayable m_clipPlayable;
         #endregion
 
         #region Public API
         public AnimationHandler(Animator _animComponent, AnimationHandlerData _data)
         {
-            AnimatorComponent = _animComponent;
-            Data = _data;
+            InitializeAnimator(_animComponent, _data);
+            InitializePlayableGraph();
         }
         public void CleanupHandler()
         {
-            AnimatorComponent = null;
+            Cleanup();
         }
         public void PlayAnimationClip(AnimationClip _clip)
         {
+            SetAnimationClip(_clip);
+            PlayCurrentAnimation();
+        }
+        #endregion
 
+        #region Utility
+        private void InitializeAnimator(Animator _animComponent, AnimationHandlerData _data)
+        {
+            m_animComponent = _animComponent;
+            m_data = _data;
+        }
+        private void InitializePlayableGraph()
+        {
+            m_playableGraph = PlayableGraph.Create();
+            m_playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+            m_playableOutput = AnimationPlayableOutput.Create(m_playableGraph, "CombatAnimation", m_animComponent);
+        }
+        private void SetAnimationClip(AnimationClip _clip)
+        {
+            m_currentClip = _clip;
+            m_clipPlayable = AnimationClipPlayable.Create(m_playableGraph, _clip);
+            m_playableOutput.SetSourcePlayable(m_clipPlayable);
+
+        }
+        void PlayCurrentAnimation()
+        {
+            m_playableGraph.Play();
+        }
+        public void PlayAnimationManually(double _time)
+        {
+            m_clipPlayable.SetTime(_time);
+        }
+        private void Cleanup()
+        {
+            m_playableGraph.Destroy();
+            m_animComponent = null;
         }
         #endregion
 
