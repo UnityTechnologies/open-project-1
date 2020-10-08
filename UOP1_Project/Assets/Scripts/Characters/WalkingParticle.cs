9f1b5dabe646027d8f426b0be63b084c67f94326
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2020-10-08 16:30:59 +0700

    Update code quality and fixed walking particle while jumping

--- UOP1_Project/Assets/Scripts/Characters/WalkingParticle.cs
+++ UOP1_Project/Assets/Scripts/Characters/WalkingParticle.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class WalkingParticle : MonoBehaviour
 {
 
     [Tooltip("Particle prefab to be attached to the charactor in the scene.")] public ParticleSystem walkingParticlePrefab;
-    [Tooltip("Minimum walking distance to play the particle.")] public float walkingDistance;
+    [Tooltip("Minimum walking distance to play the particle.")] public float walkingDistance = 0;
 
     private Vector2 lastPosition;
     private Vector2 currentPosition;
     private ParticleSystem particleObject;
+    private CharacterController charController;
 
     private void OnEnable()
     {
         lastPosition = new Vector2(transform.position.x, transform.position.z);
         currentPosition = new Vector2(transform.position.x, transform.position.z);
-        if (!particleObject && walkingParticlePrefab)
+        charController = GetComponent<CharacterController>();
+        if (!walkingParticlePrefab)
+        {
+            Destroy(this);
+        }
+        else if (!particleObject)
         {
             particleObject = Instantiate(walkingParticlePrefab, transform);
             particleObject.Stop();
@@ -28,7 +35,7 @@ public class WalkingParticle : MonoBehaviour
         if(particleObject)
         {
             currentPosition = new Vector2(transform.position.x, transform.position.z);
-            if(Vector2.Distance(currentPosition, lastPosition) > walkingDistance)
+            if(Vector2.Distance(currentPosition, lastPosition) > walkingDistance && charController.isGrounded)
             {
                 if(!particleObject.isPlaying) particleObject.Play();
             }
@@ -39,4 +46,5 @@ public class WalkingParticle : MonoBehaviour
             lastPosition = currentPosition;
         }
     }
+
 }
