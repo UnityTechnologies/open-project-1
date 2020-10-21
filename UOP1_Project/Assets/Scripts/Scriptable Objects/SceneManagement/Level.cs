028e583e45a6c572e4dcece78ff617a63a84af74
Unknown <amel.negra@unity3d.com>
2020-10-21 16:42:49 +0200

    Changed Level to Location

--- UOP1_Project/Assets/Scripts/Scriptable Objects/SceneManagement/Level.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewLevel", menuName = "Scene Data/Level")]
-public class Level : GameScene
-{
-    //Settings specific to level only
-    [Header("Level specific")]
-    public int enemiesCount;
-}
---
+++ UOP1_Project/Assets/Scripts/Scriptable Objects/SceneManagement/Location.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLocation", menuName = "Scene Data/Location")]
+public class Location : GameScene
+{
+    //Settings specific to level only
+    [Header("Location specific")]
+    public int enemiesCount;
+}
