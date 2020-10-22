6f0cb6c2554de6b4fc6976195ada6907aa97805d
Gordon Arber <nodrogames@gmail.com>
2020-10-22 17:39:43 -0400

    Changed all the public fields to [SerializeField] private
    According to the Conventions document - We are to use the attribute [SerializeField] and private, instead of making them public.

--- UOP1_Project/Assets/Scripts/Characters/Character.cs
+++ UOP1_Project/Assets/Scripts/Characters/Character.cs
@@ -4,16 +4,16 @@ public class Character : MonoBehaviour
 {
 	private CharacterController characterController;
 
-	[Tooltip("Horizontal XZ plane speed multiplier")] public float speed = 8f;
-	[Tooltip("Smoothing for rotating the character to their movement direction")] public float turnSmoothTime = 0.2f;
-	[Tooltip("General multiplier for gravity (affects jump and freefall)")] public float gravityMultiplier = 5f;
-	[Tooltip("The initial upwards push when pressing jump. This is injected into verticalMovement, and gradually cancelled by gravity")] public float initialJumpForce = 10f;
-	[Tooltip("How long can the player hold the jump button")] public float jumpInputDuration = .4f;
-	[Tooltip("Represents how fast gravityContributionMultiplier will go back to 1f. The higher, the faster")] public float gravityComebackMultiplier = 15f;
-	[Tooltip("The maximum speed reached when falling (in units/frame)")] public float maxFallSpeed = 50f;
-	[Tooltip("Each frame while jumping, gravity will be multiplied by this amount in an attempt to 'cancel it' (= jump higher)")] public float gravityDivider = .6f;
-	[Tooltip("Adjust the friction of the slope")] public float slideFriction = 0.3f;
-	[Tooltip("Starting vertical movement when falling from a platform")] public float fallingVerticalMovement = -5f;
+	[Tooltip("Horizontal XZ plane speed multiplier")] [SerializeField] private float speed = 8f;
+	[Tooltip("Smoothing for rotating the character to their movement direction")] [SerializeField] private float turnSmoothTime = 0.2f;
+	[Tooltip("General multiplier for gravity (affects jump and freefall)")] [SerializeField] private float gravityMultiplier = 5f;
+	[Tooltip("The initial upwards push when pressing jump. This is injected into verticalMovement, and gradually cancelled by gravity")] [SerializeField] private float initialJumpForce = 10f;
+	[Tooltip("How long can the player hold the jump button")] [SerializeField] private float jumpInputDuration = .4f;
+	[Tooltip("Represents how fast gravityContributionMultiplier will go back to 1f. The higher, the faster")] [SerializeField] private float gravityComebackMultiplier = 15f;
+	[Tooltip("The maximum speed reached when falling (in units/frame)")] [SerializeField] private float maxFallSpeed = 50f;
+	[Tooltip("Each frame while jumping, gravity will be multiplied by this amount in an attempt to 'cancel it' (= jump higher)")] [SerializeField] private float gravityDivider = .6f;
+	[Tooltip("Adjust the friction of the slope")] [SerializeField] private float slideFriction = 0.3f;
+	[Tooltip("Starting vertical movement when falling from a platform")] [SerializeField] private float fallingVerticalMovement = -5f;
 
 
 	private float gravityContributionMultiplier = 0f; //The factor which determines how much gravity is affecting verticalMovement
