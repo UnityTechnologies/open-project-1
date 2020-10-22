997d976df89a9338da35d05e8e51aec78ebfc3fd
circa94 <hannesklose@hotmail.de>
2020-10-22 23:25:32 +0200

    simplified member field namings

--- UOP1_Project/Assets/Scripts/Inventory/Item.cs
+++ UOP1_Project/Assets/Scripts/Inventory/Item.cs
@@ -5,16 +5,16 @@
 public abstract class Item : ScriptableObject
 {
 	[Tooltip("The Item's name")]
-	[SerializeField] private string _itemName; // TODO: Consider using a string table for localization
+	[SerializeField] private string _name; // TODO: Consider using a string table for localization
 	[Tooltip("A preview Image for the Item")]
-	[SerializeField] private Sprite _itemImage;
+	[SerializeField] private Sprite _image;
 	[Tooltip("A prefab reference for the model of the item.")]
-	[SerializeField] private MeshRenderer _itemModel; // use this as reference  prefab
+	[SerializeField] private MeshRenderer _model; // use this as reference prefab
 	[Tooltip("The Item's description")]
-	[SerializeField] [Multiline] private string _itemDescription; // TODO: Consider using a string table for localization
-	[SerializeField] private string _itemActionName; // TODO: Consider using a string table for localization
+	[SerializeField] [Multiline] private string _description; // TODO: Consider using a string table for localization
+	[SerializeField] private string _actionName; // TODO: Consider using a string table for localization
 	[Tooltip("The Item's background color in the UI")]
-	[SerializeField] private Color _itemBackgroundColor;
+	[SerializeField] private Color _backgroundColor;
 
 	// TODO: Add common functionality for properties
 }
