18f59071c86d33143957b8958bd1af5741e02a14
Hannes Klose <hannes.klose@adeccogroup.com>
2020-10-22 20:14:18 +0200

    follow code convention

--- UOP1_Project/Assets/Scripts/Inventory/Item.cs
+++ UOP1_Project/Assets/Scripts/Inventory/Item.cs
@@ -6,7 +6,7 @@ public abstract class Item : ScriptableObject
 {
 
 	// TODO: review if this enum redundant due to abstraction 
-	enum  ItemType
+	enum ItemType
 	{
 		CharacterCustomisation,
 		Ingredients,
@@ -15,17 +15,17 @@ public abstract class Item : ScriptableObject
 	}
 
 	[Tooltip("Item's Type")] 
-	[SerializeField] private ItemType itemType;
+	[SerializeField] private ItemType _itemType;
 
 	[Tooltip("The Item's name")]
-	[SerializeField] public string itemName; // TODO: Consider using a string table for localization
+	[SerializeField] private string _itemName; // TODO: Consider using a string table for localization
 	[Tooltip("A preview Image for the Item")]
-	[SerializeField] public Sprite itemImage;
+	[SerializeField] private Sprite _itemImage;
 	[Tooltip("A prefab reference for the model of the item.")]
-	[SerializeField] public MeshRenderer itemModel; // use this as reference  prefab
+	[SerializeField] private MeshRenderer _itemModel; // use this as reference  prefab
 	[Tooltip("The Item's description")]
-	[SerializeField] [Multiline] public string itemDescription; // TODO: Consider using a string table for localization
-	[SerializeField] public string itemActionName; // TODO: Consider using a string table for localization
+	[SerializeField] [Multiline] private string _itemDescription; // TODO: Consider using a string table for localization
+	[SerializeField] private string _itemActionName; // TODO: Consider using a string table for localization
 	
 	
 	// TODO: Add common functionality for properties
