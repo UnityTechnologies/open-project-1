abf170dbb9929c939f0c61489275752201fe83f3
Oded Sharon <github@odedsharon.com>
2020-10-26 03:00:10 +0200

    made changed according to request by @Ciro_unity - moved scirpable objects to it's own folder , and changed mesh renderer to gameobject

--- UOP1_Project/Assets/Scripts/Inventory/ScriptableObjects/Item.cs
+++ UOP1_Project/Assets/Scripts/Inventory/ScriptableObjects/Item.cs
@@ -9,7 +9,7 @@ public abstract class Item : ScriptableObject
 	[Tooltip("A preview Image for the Item")]
 	[SerializeField] private Sprite _image;
 	[Tooltip("A prefab reference for the model of the item.")]
-	[SerializeField] private MeshRenderer _model; // use this as reference prefab
+	[SerializeField] private GameObject _model; // use this as reference prefab
 	[Tooltip("The Item's description")]
 	[SerializeField] [Multiline] private string _description; // TODO: Consider using a string table for localization
 	[SerializeField] private string _actionName; // TODO: Consider using a string table for localization
