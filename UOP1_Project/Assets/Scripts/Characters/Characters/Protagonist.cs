4e39d4a8a2704d9dd4f82b48aca6be1b008eb553
Gordon Arber <Gordon.arber@gmail.com>
2020-10-26 20:08:26 -0400

    Character

---
+++ UOP1_Project/Assets/Scripts/Characters/Characters/Protagonist.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// <para>This class listens to the input and it deposits it on the <c>Character</c> component, ready to be used by the <c>StateMachine</c></para>
+/// </summary>
+public class Protagonist : MonoBehaviour
+{
+	[SerializeField] private InputReader _inputReader = default;
+	public Transform gameplayCamera;
+
+	private Character _charScript;
+	private Vector2 _previousMovementInput;
+
+	private void Awake()
+	{
+		_charScript = GetComponent<Character>();
+	}
+
+	//Adds listeners for events being triggered in the InputReader script
+	private void OnEnable()
+	{
+		_inputReader.jumpEvent += OnJumpInitiated;
+		_inputReader.jumpCanceledEvent += OnJumpCanceled;
+		_inputReader.moveEvent += OnMove;
+		//...
+	}
+
+	//Removes all listeners to the events coming from the InputReader script
+	private void OnDisable()
+	{
+		_inputReader.jumpEvent -= OnJumpInitiated;
+		_inputReader.jumpCanceledEvent -= OnJumpCanceled;
+		_inputReader.moveEvent -= OnMove;
+		//...
+	}
+
+	private void Update()
+	{
+		RecalculateMovement();
+	}
+
+	private void RecalculateMovement()
+	{
+		//Get the two axes from the camera and flatten them on the XZ plane
+		Vector3 cameraForward = gameplayCamera.forward;
+		cameraForward.y = 0f;
+		Vector3 cameraRight = gameplayCamera.right;
+		cameraRight.y = 0f;
+
+		//Use the two axes, modulated by the corresponding inputs, and construct the final vector
+		Vector3 adjustedMovement = cameraRight.normalized * _previousMovementInput.x +
+			cameraForward.normalized * _previousMovementInput.y;
+
+		_charScript.Move(Vector3.ClampMagnitude(adjustedMovement, 1f));
+	}
+
+	//---- EVENT LISTENERS ----
+
+	private void OnMove(Vector2 movement)
+	{
+		_previousMovementInput = movement;
+	}
+
+	private void OnJumpInitiated()
+	{
+		_charScript.Jump();
+	}
+
+	private void OnJumpCanceled()
+	{
+		_charScript.CancelJump();
+	}
+}
---
+++ UOP1_Project/Assets/Scripts/Characters/Characters/StateMachine/Conditions/IsMovingConditionSO.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "StartedMoving", menuName = "State Machines/Conditions/Started Moving")]
+public class IsMovingConditionSO : StateConditionSO
+{
+	[SerializeField] private float _treshold = 0.02f;
+
+	protected override Condition CreateCondition() => new IsMovingCondition(_treshold);
+}
+
+public class IsMovingCondition : Condition
+{
+	private float _treshold;
+	private Character _characterScript;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_characterScript = stateMachine.GetComponent<Character>();
+	}
+
+	public IsMovingCondition(float treshold)
+	{
+		_treshold = treshold;
+	}
+
+	public override bool Statement()
+	{
+		Vector3 movementVector = _characterScript.movementInput;
+		movementVector.y = 0f;
+		return movementVector.sqrMagnitude > _treshold;
+	}
+
+	public override void OnStateExit()
+	{
+		_characterScript.movementVector = Vector3.zero;
+	}
+}
