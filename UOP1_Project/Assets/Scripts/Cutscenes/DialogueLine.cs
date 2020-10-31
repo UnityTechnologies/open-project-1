1941e303e8dad747e03d5f7bba09da20bbb5352a
rainaldisatria <rainaldisatria212@gmail.com>
2020-10-31 11:23:46 +0700

    Create dialogue data type (SO)

---
+++ UOP1_Project/Assets/Scripts/Cutscenes/DialogueLine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class DialogueLine
+{
+	public Sprite Figure;
+	public string ActorName;
+	[TextArea(3, 3)]
+	public string Sentence;
+}
