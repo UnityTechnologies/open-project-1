c1aca3610d73fa83acb43e72e895f349c4250fbf
rainaldisatria <rainaldisatria212@gmail.com>
2020-10-31 20:03:04 +0700

    More clean up

--- UOP1_Project/Assets/Scripts/Cutscenes/Dialogue/DialogueLine.cs
+++ UOP1_Project/Assets/Scripts/Cutscenes/Dialogue/DialogueLine.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 [Serializable]
 public class DialogueLine
 {
-	public Actor Actor;
-	public string NameOverride;
-	public Sprite FigureOverride;
-	[TextArea(3, 3)]
-	public string Sentence;
+	public Actor Actor { get => _actor; }
+	public string NameOverride { get => _nameOverride; }
+	public Sprite FigureOverride { get => _figureOverride; }
+	public string Sentence { get => _sentence; }
+
+	[SerializeField] private Actor _actor;
+	[SerializeField] private string _nameOverride;
+	[SerializeField] private Sprite _figureOverride;
+	[SerializeField] [TextArea(3, 3)] private string _sentence;
 }
