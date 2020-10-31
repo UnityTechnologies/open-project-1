b19f0388af84feba413661cf0645f208eb68d520
rainaldisatria <rainaldisatria212@gmail.com>
2020-10-31 11:32:16 +0700

    Create CutsceneData.cs to hold information about the dialogue that wants to be played and the on end event

---
+++ UOP1_Project/Assets/Scripts/Cutscenes/CutsceneData.cs
@@ -0,0 +1,11 @@
+﻿using System; 
+using UnityEngine;
+
+[Serializable]
+public class CutsceneData
+{
+	// We can make this a list
+	public DialogueData DialogueData;
+	[Tooltip("Event placeholder that will be called after DialogueData reached its end.")]
+	public string Event; 
+}
