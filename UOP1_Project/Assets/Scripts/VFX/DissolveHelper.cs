cadfeffd0ef705d0eee4d75096bddf27567ee74b
Erdei Benjámin <erdei_benjamin@hotmail.com>
2020-11-08 11:52:29 +0100

    Cleaned up version
    Hopefully this is fine like this, I created another scene where I can test out effects without interfering with the ArtShowcase scene.

---
+++ UOP1_Project/Assets/Scripts/VFX/DissolveHelper.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveHelper : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem _dissolveParticles;
+    [SerializeField]
+    MeshRenderer _renderer;
+    [SerializeField]
+    float dissolveTime = 1;
+
+    public InputReader inputReader;
+
+    MaterialPropertyBlock materialPropertyBlock;
+
+    void Start() {
+        if (materialPropertyBlock == null) 
+        {
+            materialPropertyBlock = new MaterialPropertyBlock();
+        }
+        setParticleSystem();
+    }
+
+    public void triggerDissolve() 
+    {
+        StartCoroutine(DissolveCoroutine());
+    }
+
+    void OnValidate() 
+    {
+        setParticleSystem();
+    }
+
+    void setParticleSystem()
+    {
+        var mainModule = _dissolveParticles.main;
+        mainModule.duration = dissolveTime - 0.3f;
+    }
+
+    public IEnumerator DissolveCoroutine() 
+    {
+        float normalizedDeltaTime = 0;
+
+        _dissolveParticles.Play();
+        while(normalizedDeltaTime < dissolveTime) 
+        {
+            // dissolve logic
+            normalizedDeltaTime += Time.deltaTime;
+            float remappedValue = VFXUtil.RemapValue(normalizedDeltaTime, 0, dissolveTime, 0, 1);
+            materialPropertyBlock.SetFloat("_Dissolve", remappedValue);
+            _renderer.SetPropertyBlock(materialPropertyBlock);
+            yield return null;
+        }
+    }
+}
---
+++ UOP1_Project/Assets/Scripts/VFX/VFXUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXUtil : MonoBehaviour
+{
+    public static float RemapValue(float value, float low1, float high1, float low2, float high2) 
+    {
+        float x =  low2 + (value - low1) * (high2 - low2) / (high1 - low1);
+        return x;
+    }
+}
