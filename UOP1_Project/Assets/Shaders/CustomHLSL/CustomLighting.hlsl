061bbd7ef2c98ffea005ec4f9d33f18ebb80c634
Matteo Baraldi <ma7730@hotmail.it>
2020-11-11 17:45:26 +0100

    OP Devlog #1: Toon shading (#173)
    * upgrade to 2020.2
    
    * add else condition in CustomLighting
    
    * fix missing material in ToonShading scene
    
    Co-authored-by: doJester13 <minarini.work@gmail.com>
    Co-authored-by: doJester13 <minairni.work@gmail.com>

--- UOP1_Project/Assets/Shaders/CustomHLSL/CustomLighting.hlsl
+++ UOP1_Project/Assets/Shaders/CustomHLSL/CustomLighting.hlsl
@@ -3,7 +3,7 @@
 
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Direction = float3(0.5, 0.5, 0);
     Color = 1;
     DistanceAtten = 1;
@@ -18,20 +18,20 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
 
 	#if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
 		ShadowAtten = 1.0h;
-	#endif
-
-	ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-	float shadowStrength = GetMainLightShadowStrength();
-	ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
-	sampler_MainLightShadowmapTexture),
-	shadowSamplingData, shadowStrength, false);
+	#else
+        ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+        float shadowStrength = GetMainLightShadowStrength();
+        ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
+        sampler_MainLightShadowmapTexture),
+        shadowSamplingData, shadowStrength, false);
+    #endif
 #endif
 }
 
 
 void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, float3 Color, float3 WorldNormal, float3 WorldView, out float3 Out)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Out = 0;
 #else
     Smoothness = exp2(10 * Smoothness + 1);
