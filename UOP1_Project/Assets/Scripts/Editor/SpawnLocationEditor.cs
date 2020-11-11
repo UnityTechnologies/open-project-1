e90f9ea4bbb6e73739db2d60fef7d2cd60a055c4
Dmitriy Tarasov <tarasov65536@gmail.com>
2020-11-11 10:47:09 +0200

    fix: Project won't compile due to a reference to UnityEditor (#157)

--- UOP1_Project/Assets/Scripts/Editor/SpawnLocationEditor.cs
+++ UOP1_Project/Assets/Scripts/Editor/SpawnLocationEditor.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 [CustomEditor(typeof(ClickToPlaceHelper))]
 public class ClickToPlaceHelperEditor : Editor
 {
+	private ClickToPlaceHelper helper => target as ClickToPlaceHelper;
+
 	public override void OnInspectorGUI()
 	{
 		base.OnInspectorGUI();
 
-		ClickToPlaceHelper myTarget = target as ClickToPlaceHelper;
-
-		if (GUILayout.Button("Place at Mouse cursor"))
+		if (GUILayout.Button("Place at Mouse cursor") && !helper.targeting)
 		{
-			myTarget.SetSpawnLocationAtCursor();
+			helper.BeginTargeting();
+			SceneView.duringSceneGui += DuringSceneGui;
+		}
+	}
+
+	private void DuringSceneGui(SceneView sceneView)
+	{
+		Event currentGUIEvent = Event.current;
+
+		Vector3 mousePos = currentGUIEvent.mousePosition;
+		float pixelsPerPoint = EditorGUIUtility.pixelsPerPoint;
+		mousePos.y = sceneView.camera.pixelHeight - mousePos.y * pixelsPerPoint;
+		mousePos.x *= pixelsPerPoint;
+
+		Ray ray = sceneView.camera.ScreenPointToRay(mousePos);
+
+		if (Physics.Raycast(ray, out RaycastHit hit))
+		{
+			helper.UpdateTargeting(hit.point);
+		}
+
+		switch (currentGUIEvent.type)
+		{
+			case EventType.MouseMove:
+				HandleUtility.Repaint();
+				break;
+			case EventType.MouseDown:
+				if (currentGUIEvent.button == 0) // Wait for Left mouse button down
+				{
+					helper.EndTargeting();
+					SceneView.duringSceneGui -= DuringSceneGui;
+					currentGUIEvent.Use(); // This consumes the event, so that other controls/buttons won't be able to use it
+				}
+				break;
 		}
 	}
 }
