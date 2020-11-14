efa8b40b2b297743a52c6a11e211425b2a25185c
kgc00 <kirbygc00@gmail.com>
2020-11-13 16:49:54 -0800

    debug

---
+++ UOP1_Project/Assets/Scripts/Menu/DebugMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class DebugMenu : MonoBehaviour
+{
+	[SerializeField] private GameObject _menuPrefab;
+	private GameObject _menuInstance;
+	[SerializeField] private InputReader _inputReader;
+	private void OnEnable()
+	{
+		_inputReader.pauseEvent += OpenMenu;
+		_inputReader.Menu.CloseMenuEvent += CloseMenu;
+	}
+
+	private void OnDisable()
+	{
+		_inputReader.pauseEvent -= OpenMenu;
+		_inputReader.Menu.CloseMenuEvent -= CloseMenu;
+	}
+
+	private void OpenMenu()
+	{
+		if (_menuInstance == null) _menuInstance = Instantiate(_menuPrefab);
+		_menuInstance.SetActive(true);
+		_inputReader.EnableMenuInput();
+	}
+
+	private void CloseMenu()
+	{
+		_menuInstance.SetActive(false);
+		_inputReader.EnableGameplayInput();
+	}
+}
