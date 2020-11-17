d55391c906b1d8d7cad9012c395e02b480cfa0a0
Casey Hofland <hofland.casey@gmail.com>
2020-11-17 03:12:56 +0100

    Game Settings and preview implementation
    Game Settings makes it easy to create setting and edit UI without writing additional logic! Check out the OptionsMenu - Preview scene for this.
    
    As a test, try adding a Shadow Quality Dropdown to the scene and have it display the "Hard Only" option.
    
    Support for URP and the Input System is coming.

---
+++ UOP1_Project/Assets/Scripts/Settings/SettingToText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using GameSettings;
+
+[ExecuteAlways]
+public class SettingToText : MonoBehaviour
+{
+	public TextMeshProUGUI text;
+	public GameSetting gameSetting;
+
+	private void LateUpdate()
+	{
+		text.text = gameSetting.objectValue.ToString();
+	}
+}
