d2d5691178393194473deb1ed862ca9056bcb7a2
DeivSky <david.lischinsky@outlook.com>
2020-11-27 17:36:17 -0300

    Changed to isPlayingOrWillChangePlaymode instead of isPlaying

--- UOP1_Project/Assets/Scripts/StateMachine/Editor/Utilities/InitOnlyAttributeDrawer.cs
+++ UOP1_Project/Assets/Scripts/StateMachine/Editor/Utilities/InitOnlyAttributeDrawer.cs
@@ -6,12 +6,12 @@ namespace UOP1.StateMachine.Editor
 	[CustomPropertyDrawer(typeof(InitOnlyAttribute))]
 	public class InitOnlyAttributeDrawer : PropertyDrawer
 	{
-		private static readonly string _text = "Changes to this parameter will only take effect on new state machine instances, or the next time you enter play mode.";
+		private static readonly string _text = "Changes to this parameter during play mode will only take effect on new state machine instances, or the next time you enter play mode.";
 		private static readonly GUIStyle _style = new GUIStyle(GUI.skin.GetStyle("helpbox")) { padding = new RectOffset(5, 5, 5, 5) };
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			if (EditorApplication.isPlaying)
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
 			{
 				position.height = _style.CalcHeight(new GUIContent(_text), EditorGUIUtility.currentViewWidth);
 				EditorGUI.HelpBox(position, _text, MessageType.Info);
@@ -26,7 +26,7 @@ namespace UOP1.StateMachine.Editor
 		{
 			float height = EditorGUI.GetPropertyHeight(property, label);
 
-			if (EditorApplication.isPlaying)
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
 			{
 				height += _style.CalcHeight(new GUIContent(_text), EditorGUIUtility.currentViewWidth)
 					+ EditorGUIUtility.standardVerticalSpacing * 4;
