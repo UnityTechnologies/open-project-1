fa6606ce9ad326273b0fa006b7db9bb93db0f502
neon-age <alexander.neonage@gmail.com>
2020-11-27 20:13:20 +0000

    [Bot] Automated dotnet-format update

--- UOP1_Project/Assets/Scripts/Editor/GameObjectPreview.cs
+++ UOP1_Project/Assets/Scripts/Editor/GameObjectPreview.cs
@@ -6,79 +6,79 @@ using UnityEngine;
 namespace UOP1.EditorTools
 {
 	internal class GameObjectPreview
-    {
-        private static MethodInfo getPreviewDataMethod;
-        private static FieldInfo renderUtilityField;
+	{
+		private static MethodInfo getPreviewDataMethod;
+		private static FieldInfo renderUtilityField;
 
-        private Rect renderRect;
-        private Color light0Color;
-        private Color light1Color;
-        private PreviewRenderUtility renderUtility;
+		private Rect renderRect;
+		private Color light0Color;
+		private Color light1Color;
+		private PreviewRenderUtility renderUtility;
 
-        private Editor dummyEditor;
-        private Editor cachedEditor;
+		private Editor dummyEditor;
+		private Editor cachedEditor;
 
-        public RenderTexture outputTexture;
+		public RenderTexture outputTexture;
 
-        [InitializeOnLoadMethod]
-        private static void OnInitialize()
-        {
-            var gameObjectInspectorType = typeof(Editor).Assembly.GetType("UnityEditor.GameObjectInspector");
-            var previewDataType = gameObjectInspectorType.GetNestedType("PreviewData", BindingFlags.NonPublic);
+		[InitializeOnLoadMethod]
+		private static void OnInitialize()
+		{
+			var gameObjectInspectorType = typeof(Editor).Assembly.GetType("UnityEditor.GameObjectInspector");
+			var previewDataType = gameObjectInspectorType.GetNestedType("PreviewData", BindingFlags.NonPublic);
 
-            getPreviewDataMethod = gameObjectInspectorType.GetMethod("GetPreviewData", BindingFlags.NonPublic | BindingFlags.Instance);
-            renderUtilityField = previewDataType.GetField("renderUtility", BindingFlags.Public | BindingFlags.Instance);
-        }
+			getPreviewDataMethod = gameObjectInspectorType.GetMethod("GetPreviewData", BindingFlags.NonPublic | BindingFlags.Instance);
+			renderUtilityField = previewDataType.GetField("renderUtility", BindingFlags.Public | BindingFlags.Instance);
+		}
 
-        public void CreatePreviewForTarget(GameObject target)
-        {
-	        if (!dummyEditor)
-		        dummyEditor = Editor.CreateEditor(target);
+		public void CreatePreviewForTarget(GameObject target)
+		{
+			if (!dummyEditor)
+				dummyEditor = Editor.CreateEditor(target);
 
-            if (!cachedEditor || cachedEditor.target != target)
-            {
-	            renderUtility = null;
-	            // There is a bug that breaks previews and Prefab mode after creating too many editors.
-	            // Simply using CreateCachedEditor is fixing that problem.
-	            Editor.CreateCachedEditor(target, dummyEditor.GetType(), ref cachedEditor);
-            }
-        }
+			if (!cachedEditor || cachedEditor.target != target)
+			{
+				renderUtility = null;
+				// There is a bug that breaks previews and Prefab mode after creating too many editors.
+				// Simply using CreateCachedEditor is fixing that problem.
+				Editor.CreateCachedEditor(target, dummyEditor.GetType(), ref cachedEditor);
+			}
+		}
 
-        public void RenderInteractivePreview(Rect rect)
-        {
-            if (!cachedEditor)
-                return;
+		public void RenderInteractivePreview(Rect rect)
+		{
+			if (!cachedEditor)
+				return;
 
-            if (renderUtility == null || renderUtility.lights[0] == null)
-            {
-                var previewData = getPreviewDataMethod.Invoke(cachedEditor, null);
-                renderUtility = renderUtilityField.GetValue(previewData) as PreviewRenderUtility;
+			if (renderUtility == null || renderUtility.lights[0] == null)
+			{
+				var previewData = getPreviewDataMethod.Invoke(cachedEditor, null);
+				renderUtility = renderUtilityField.GetValue(previewData) as PreviewRenderUtility;
 
-                light0Color = renderUtility.lights[0].color;
-                light1Color = renderUtility.lights[1].color;
-            }
+				light0Color = renderUtility.lights[0].color;
+				light1Color = renderUtility.lights[1].color;
+			}
 
-            renderUtility.lights[0].color = light0Color * 1.6f;
-            renderUtility.lights[1].color = light1Color * 6f;
-            var backColor = renderUtility.camera.backgroundColor;
-            renderUtility.camera.backgroundColor = new Color(backColor.r, backColor.g, backColor.b, 0);
-            renderUtility.camera.clearFlags = CameraClearFlags.Depth;
+			renderUtility.lights[0].color = light0Color * 1.6f;
+			renderUtility.lights[1].color = light1Color * 6f;
+			var backColor = renderUtility.camera.backgroundColor;
+			renderUtility.camera.backgroundColor = new Color(backColor.r, backColor.g, backColor.b, 0);
+			renderUtility.camera.clearFlags = CameraClearFlags.Depth;
 
-            var color = GUI.color;
+			var color = GUI.color;
 
-            // Hide default preview texture, since it has no alpha blending
-            GUI.color = new Color(1, 1, 1, 0);
+			// Hide default preview texture, since it has no alpha blending
+			GUI.color = new Color(1, 1, 1, 0);
 
-            cachedEditor.OnPreviewGUI(rect, null);
+			cachedEditor.OnPreviewGUI(rect, null);
 
-            GUI.color = color;
+			GUI.color = color;
 
-            outputTexture = renderUtility.camera.targetTexture;
-        }
+			outputTexture = renderUtility.camera.targetTexture;
+		}
 
-        public void DrawPreviewTexture(Rect rect)
-        {
-	        GUI.DrawTexture(rect, outputTexture, ScaleMode.ScaleToFit, true, 0);
-        }
-    }
+		public void DrawPreviewTexture(Rect rect)
+		{
+			GUI.DrawTexture(rect, outputTexture, ScaleMode.ScaleToFit, true, 0);
+		}
+	}
 }
--- UOP1_Project/Assets/Scripts/Editor/ReplacePrefabSearchPopup.cs
+++ UOP1_Project/Assets/Scripts/Editor/ReplacePrefabSearchPopup.cs
@@ -11,182 +11,184 @@ using static UnityEngine.Application;
 namespace UOP1.EditorTools.Replacer
 {
 	internal class ReplacePrefabSearchPopup : EditorWindow
-    {
-	    private const float previewHeight = 128;
+	{
+		private const float previewHeight = 128;
 
-    	private class ViewState : ScriptableObject
-    	{
-    		public TreeViewState treeViewState = new TreeViewState();
-    	}
+		private class ViewState : ScriptableObject
+		{
+			public TreeViewState treeViewState = new TreeViewState();
+		}
 
-    	private static ReplacePrefabSearchPopup window;
-        private static Styles styles;
+		private static ReplacePrefabSearchPopup window;
+		private static Styles styles;
 
-        private static Event evt => Event.current;
-    	private static string assetPath => Path.Combine(dataPath.Remove(dataPath.Length - 7, 7), "Library", "ReplacePrefabTreeState.asset");
+		private static Event evt => Event.current;
+		private static string assetPath => Path.Combine(dataPath.Remove(dataPath.Length - 7, 7), "Library", "ReplacePrefabTreeState.asset");
 
-        private bool hasSelection => tree.state.selectedIDs.Count > 0;
-        private int selectedId => tree.state.selectedIDs[0];
-        private GameObject instance => EditorUtility.InstanceIDToObject(selectedId) as GameObject;
+		private bool hasSelection => tree.state.selectedIDs.Count > 0;
+		private int selectedId => tree.state.selectedIDs[0];
+		private GameObject instance => EditorUtility.InstanceIDToObject(selectedId) as GameObject;
 
-    	private SearchField searchField;
-    	private ReplacePrefabTreeView tree;
-    	private ViewState viewState;
+		private SearchField searchField;
+		private ReplacePrefabTreeView tree;
+		private ViewState viewState;
 
-        private Vector2 startPos;
-        private Vector2 startSize;
-        private Rect itemRect;
+		private Vector2 startPos;
+		private Vector2 startSize;
+		private Rect itemRect;
 
-        private GameObjectPreview selectionPreview = new GameObjectPreview();
+		private GameObjectPreview selectionPreview = new GameObjectPreview();
 
-    	public static void Show(Rect itemRect, Vector2 position, Vector2 size)
-    	{
-            var windows = Resources.FindObjectsOfTypeAll<ReplacePrefabSearchPopup>();
-            window = windows.Length != 0 ? windows[0] : CreateInstance<ReplacePrefabSearchPopup>();
+		public static void Show(Rect itemRect, Vector2 position, Vector2 size)
+		{
+			var windows = Resources.FindObjectsOfTypeAll<ReplacePrefabSearchPopup>();
+			window = windows.Length != 0 ? windows[0] : CreateInstance<ReplacePrefabSearchPopup>();
 
-            window.Init();
+			window.Init();
 
-            window.startPos = position;
-            window.startSize = size;
+			window.startPos = position;
+			window.startSize = size;
 
-            window.position = new Rect(position, size);
-            window.SetCorrectSize();
-            //window.ShowAsDropDown(new Rect(position, Vector2.zero), size);
+			window.position = new Rect(position, size);
+			window.SetCorrectSize();
+			//window.ShowAsDropDown(new Rect(position, Vector2.zero), size);
 
-            // This type of window supports resizing, but is also persistent, so we need to close it manually
-            window.ShowPopup();
+			// This type of window supports resizing, but is also persistent, so we need to close it manually
+			window.ShowPopup();
 
-    		//onSelectEntry += _ => window.Close();
-            window.itemRect = itemRect;
-    	}
+			//onSelectEntry += _ => window.Close();
+			window.itemRect = itemRect;
+		}
 
-    	private void Init()
-    	{
-    		viewState = CreateInstance<ViewState>();
+		private void Init()
+		{
+			viewState = CreateInstance<ViewState>();
 
-    		if (File.Exists(assetPath))
-    			FromJsonOverwrite(File.ReadAllText(assetPath), viewState);
+			if (File.Exists(assetPath))
+				FromJsonOverwrite(File.ReadAllText(assetPath), viewState);
 
-    		tree = new ReplacePrefabTreeView(viewState.treeViewState);
-            tree.onSelectEntry += OnSelectEntry;
+			tree = new ReplacePrefabTreeView(viewState.treeViewState);
+			tree.onSelectEntry += OnSelectEntry;
 
-            AssetPreview.SetPreviewTextureCacheSize(tree.RowsCount);
+			AssetPreview.SetPreviewTextureCacheSize(tree.RowsCount);
 
-    		searchField = new SearchField();
-    		searchField.downOrUpArrowKeyPressed += tree.SetFocusAndEnsureSelectedItem;
-    		searchField.SetFocus();
-    	}
+			searchField = new SearchField();
+			searchField.downOrUpArrowKeyPressed += tree.SetFocusAndEnsureSelectedItem;
+			searchField.SetFocus();
+		}
 
-        private void OnSelectEntry(GameObject prefab)
-        {
-	        ReplaceTool.ReplaceSelectedObjects(Selection.gameObjects, prefab);
-        }
+		private void OnSelectEntry(GameObject prefab)
+		{
+			ReplaceTool.ReplaceSelectedObjects(Selection.gameObjects, prefab);
+		}
 
-        private void OnEnable()
-        {
-	        Init();
-        }
+		private void OnEnable()
+		{
+			Init();
+		}
 
-        public new void Close()
-        {
-	        SaveState();
-	        base.Close();
-        }
+		public new void Close()
+		{
+			SaveState();
+			base.Close();
+		}
 
-        private void SaveState()
-        {
-	        File.WriteAllText(assetPath, ToJson(viewState));
-        }
+		private void SaveState()
+		{
+			File.WriteAllText(assetPath, ToJson(viewState));
+		}
 
-    	private void OnGUI()
-    	{
-	        if (evt.type == EventType.KeyDown && evt.keyCode == KeyCode.Escape)
-	        {
-		        if (tree.hasSearch)
-			        tree.searchString = "";
-		        else
+		private void OnGUI()
+		{
+			if (evt.type == EventType.KeyDown && evt.keyCode == KeyCode.Escape)
+			{
+				if (tree.hasSearch)
+					tree.searchString = "";
+				else
 					Close();
-	        }
+			}
 
-	        if (focusedWindow != this)
+			if (focusedWindow != this)
 				Close();
 
-	        if (styles == null)
-		        styles = new Styles();
+			if (styles == null)
+				styles = new Styles();
 
-    		DoToolbar();
-    		DoTreeView();
-            DoSelectionPreview();
-        }
+			DoToolbar();
+			DoTreeView();
+			DoSelectionPreview();
+		}
 
-    	void DoToolbar()
-    	{
-    		tree.searchString = searchField.OnToolbarGUI(tree.searchString);
-            GUILayout.Space(-2);
+		void DoToolbar()
+		{
+			tree.searchString = searchField.OnToolbarGUI(tree.searchString);
+			GUILayout.Space(-2);
 
-            var headerRect = GUILayoutUtility.GetRect(0, itemRect.width, 0, itemRect.height);
+			var headerRect = GUILayoutUtility.GetRect(0, itemRect.width, 0, itemRect.height);
 
-            if (Event.current.type == EventType.Repaint)
+			if (Event.current.type == EventType.Repaint)
 				EditorGUI.DrawRect(headerRect, styles.headerColor);
 
-    		GUILayout.Label("Replace With...", styles.headerLabel);
-    	}
+			GUILayout.Label("Replace With...", styles.headerLabel);
+		}
 
-    	void DoTreeView()
-    	{
-    		var rect = GUILayoutUtility.GetRect(0, 10000, 0, 10000);
-    		rect.x += 2;
-    		rect.width -= 4;
+		void DoTreeView()
+		{
+			var rect = GUILayoutUtility.GetRect(0, 10000, 0, 10000);
+			rect.x += 2;
+			rect.width -= 4;
 
-    		rect.y += 2;
-    		rect.height -= 4;
+			rect.y += 2;
+			rect.height -= 4;
 
-    		tree.OnGUI(rect);
-    	}
+			tree.OnGUI(rect);
+		}
 
-        void DoSelectionPreview()
-        {
-	        SetCorrectSize();
+		void DoSelectionPreview()
+		{
+			SetCorrectSize();
 
-	        if (hasSelection && instance)
-	        {
-		        var previewRect = GUILayoutUtility.GetRect(position.width, previewHeight);
+			if (hasSelection && instance)
+			{
+				var previewRect = GUILayoutUtility.GetRect(position.width, previewHeight);
 
-		        selectionPreview.CreatePreviewForTarget(instance);
+				selectionPreview.CreatePreviewForTarget(instance);
 
-		        selectionPreview.RenderInteractivePreview(previewRect);
+				selectionPreview.RenderInteractivePreview(previewRect);
 
-		        selectionPreview.DrawPreviewTexture(previewRect);
-	        }
-        }
+				selectionPreview.DrawPreviewTexture(previewRect);
+			}
+		}
 
-        private void SetCorrectSize()
-        {
-	        if (hasSelection && instance)
-		        SetSize(startSize.x, startSize.y + previewHeight);
-	        else
-		        SetSize(startSize.x, startSize.y);
-        }
+		private void SetCorrectSize()
+		{
+			if (hasSelection && instance)
+				SetSize(startSize.x, startSize.y + previewHeight);
+			else
+				SetSize(startSize.x, startSize.y);
+		}
 
-        private void SetSize(float width, float height)
-        {
+		private void SetSize(float width, float height)
+		{
 			position = new Rect(startPos.x, startPos.y, width, height);
-        }
+		}
 
-        private class Styles
-        {
-	        public Color headerColor = isProSkin ? new Color32(77, 77, 77, 255) : new Color32(174, 174, 174, 255);
+		private class Styles
+		{
+			public Color headerColor = isProSkin ? new Color32(77, 77, 77, 255) : new Color32(174, 174, 174, 255);
 
-	        public GUIStyle header = new GUIStyle("AC BoldHeader")
-	        {
-		        fixedHeight = 0, stretchWidth = true,
-		        margin = new RectOffset(0, 0, 0, 0)
-	        };
+			public GUIStyle header = new GUIStyle("AC BoldHeader")
+			{
+				fixedHeight = 0,
+				stretchWidth = true,
+				margin = new RectOffset(0, 0, 0, 0)
+			};
 
-	        public GUIStyle headerLabel = new GUIStyle(EditorStyles.centeredGreyMiniLabel)
-	        {
-		        fontSize = 11, fontStyle = FontStyle.Bold
-	        };
-        }
-    }
+			public GUIStyle headerLabel = new GUIStyle(EditorStyles.centeredGreyMiniLabel)
+			{
+				fontSize = 11,
+				fontStyle = FontStyle.Bold
+			};
+		}
+	}
 }
