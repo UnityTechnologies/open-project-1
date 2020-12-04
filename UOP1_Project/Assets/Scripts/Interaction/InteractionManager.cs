46554247336c3ac45915b0fadbfd0d231f38e22f
Amel <amel.negra@unity3d.com>
2020-12-04 18:03:56 +0100

    Added interaction events and updated interaction manager

--- UOP1_Project/Assets/Scripts/Interaction/InteractionManager.cs
+++ UOP1_Project/Assets/Scripts/Interaction/InteractionManager.cs
@@ -4,7 +4,8 @@ public enum Interaction { None = 0, PickUp, Cook, Talk };
 
 public class InteractionManager : MonoBehaviour
 {
-	private Interaction _interaction;
+	private Interaction _potentialInteraction;
+	[HideInInspector] public Interaction currentInteraction;
 	[SerializeField] private InputReader _inputReader = default;
 	//To store the object we are currently interacting with
 	private GameObject _currentInteractableObject = null;
@@ -12,13 +13,12 @@ public class InteractionManager : MonoBehaviour
 	//Item for pickup?
 	//Character (or other relevant type) for talk?
 
-
 	//Events for the different interaction types
 	[Header("Broadcasting on")]
 	[SerializeField] private GameObjectEventChannelSO _onObjectPickUp = default;
 	//double check with the action name we will show on the UI (because we will not really starting cooking but showing the UI?)
 	[SerializeField] private VoidEventChannelSO _onCookingStart = default;
-	[SerializeField] private GameObjectEventChannelSO _startTalking = default;
+	[SerializeField] private DialogueEventChannelSo _startTalking = default;
 	//UI event
 	[SerializeField] private InteractionUIEventChannelSO _toggleInteractionUI = default;
 	[Header("Listening to")]
@@ -41,12 +41,12 @@ public class InteractionManager : MonoBehaviour
 	void OnInteractionEnd()
 	{
 		_inputReader.EnableGameplayInput();
-		switch (_interaction)
+		switch (_potentialInteraction)
 		{
 			//we show it after cooking or talking, in case player want to interact again
 			case Interaction.Cook:
 			case Interaction.Talk:
-				_toggleInteractionUI.RaiseEvent(true, _interaction);
+				_toggleInteractionUI.RaiseEvent(true, _potentialInteraction);
 				Debug.Log("Display interaction UI");
 				break;
 			default:
@@ -56,18 +56,19 @@ public class InteractionManager : MonoBehaviour
 
 	void OnInteractionButtonPress()
 	{
-		switch (_interaction)
+		switch (_potentialInteraction)
 		{
 			case Interaction.None:
 				return;
 			case Interaction.PickUp:
 				if(_currentInteractableObject != null)
 				{
-					//pass the item SO to the UI? we do it once we merge with inventory stuff
+					//raise an event with an item as parameter (to add object to inventory)
 					//Item currentItem = currentInteractableObject.GetComponent<Pickable>.item;
-					_onObjectPickUp.RaiseEvent(_currentInteractableObject);
+					//_onObjectPickUp.RaiseEvent(currentItem);
 					Debug.Log("PickUp event raised");
-					//bool for SM
+					//set current interaction for state machine
+					currentInteraction = Interaction.PickUp;
 				}
 				//destroy the GO
 				Destroy(_currentInteractableObject);
@@ -77,15 +78,20 @@ public class InteractionManager : MonoBehaviour
 				Debug.Log("Cooking event raised");
 				//Change the action map
 				_inputReader.EnableUIInput();
+				//set current interaction for state machine
+				currentInteraction = Interaction.Cook;
 				break;
 			case Interaction.Talk:
 				if (_currentInteractableObject != null)
 				{
-					//raise an event with an actor: DialogueEventChannelSo
-					_startTalking.RaiseEvent(_currentInteractableObject);
+					//raise an event with an actor as parameter
+					//Item currentItem = currentInteractableObject.GetComponent<Dialogue>.Actor;
+					//_startTalking.RaiseEvent(currentItem);
 					Debug.Log("talk event raised");
 					//Change the action map
 					_inputReader.EnableDialogueInput();
+					//set current interaction for state machine
+					currentInteraction = Interaction.Talk;
 				}
 				break;
 			default:
@@ -97,19 +103,19 @@ public class InteractionManager : MonoBehaviour
 	{
 		if (other.CompareTag("Pickable"))
 		{
-			_interaction = Interaction.PickUp;
+			_potentialInteraction = Interaction.PickUp;
 			Debug.Log("I triggered a pickable object!");
 			DisplayInteractionUI();
 		}
 		else if (other.CompareTag("CookingPot"))
 		{
-			_interaction = Interaction.Cook;
+			_potentialInteraction = Interaction.Cook;
 			Debug.Log("I triggered a cooking pot!");
 			DisplayInteractionUI();
 		}
 		else if (other.CompareTag("NPC"))
 		{
-			_interaction = Interaction.Talk;
+			_potentialInteraction = Interaction.Talk;
 			Debug.Log("I triggered an NPC!");
 			DisplayInteractionUI();
 		}
@@ -119,7 +125,7 @@ public class InteractionManager : MonoBehaviour
 	private void DisplayInteractionUI ()
 	{
 		//Raise event to display UI
-		_toggleInteractionUI.RaiseEvent(true, _interaction);
+		_toggleInteractionUI.RaiseEvent(true, _potentialInteraction);
 	}
 
 	private void OnTriggerExit(Collider other)
@@ -129,10 +135,7 @@ public class InteractionManager : MonoBehaviour
 
 	private void ResetInteraction()
 	{
-		_interaction = Interaction.None;
+		_potentialInteraction = Interaction.None;
 		_currentInteractableObject = null;
 	}
 }
-
-
-//I think the dialogue and UI should switch the action maps? for now I do here but maybe to change later
