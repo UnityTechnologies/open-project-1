5d814874f470880c5b5ec85007682d72eabaa5d8
HarshNarayanJha <pawanjha2802.pj@gmail.com>
2020-12-10 16:41:41 +0530

    Added an Editor Window to load Scenes quickly

---
+++ UOP1_Project/Assets/Scripts/Editor/EditorSceneLoader/LoadSceneWindow.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+class LoadSceneWindow : EditorWindow 
+{
+    [SerializeField]
+    public AllScenesHolderSO scenesData;
+
+    public SceneButtons[] sceneButtons = null;
+
+    Vector2 scrollView;
+
+    [MenuItem("ChopChop/Scene Management/Scene Loader Window")]
+    public static void ShowWindow() 
+    {
+        EditorWindow.GetWindow(typeof(LoadSceneWindow), false, "Load A Scene", true);
+    }
+
+    void OnGUI() 
+    {
+        GUILayout.Space(30);
+        scenesData = (AllScenesHolderSO) EditorGUILayout.ObjectField("Scenes Index", scenesData, typeof(AllScenesHolderSO), false);
+        GUILayout.Space(15);
+        GUILayout.Label("A small Scene Loader Window.", EditorStyles.boldLabel);
+
+        scrollView = EditorGUILayout.BeginScrollView(scrollView);
+
+        if (scenesData == null)
+            return;
+        
+        if (sceneButtons == null)
+        {
+            sceneButtons = new SceneButtons[scenesData.Scenes.Length];
+            for (int i = 0; i < sceneButtons.Length; i++)
+            {
+                sceneButtons[i] = new SceneButtons();
+                sceneButtons[i].sceneName = scenesData.Scenes[i].sceneDisplayName;
+                sceneButtons[i].scenePath = scenesData.Scenes[i].scenePath;
+            }
+        }
+
+        for (int i = 0; i < sceneButtons.Length; i++)
+        {
+            if (!EditorSceneManager.GetActiveScene().name.Equals(sceneButtons[i].sceneName))
+            {
+                if (sceneButtons[i].button = GUILayout.Button(sceneButtons[i].sceneName, GUILayout.Width(250), GUILayout.Height(35)))
+                {
+                    sceneButtons[i].PressButton();
+                }
+                GUILayout.Space(20);
+            }
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+}
+
+class SceneButtons
+{
+    public bool button;
+    public string sceneName;
+    public string scenePath;
+
+    public void PressButton()
+    {
+        EditorSceneManager.OpenScene(scenePath);
+        //Debug.Log(scenePath);
+    }
+}
---
+++ UOP1_Project/Assets/Scripts/Editor/EditorSceneLoader/ScriptableObjects/AllScenesHolderSO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "AllScenesReference", menuName = "Scene Data/All Scenes Holder")]
+public class AllScenesHolderSO : ScriptableObject
+{
+    [Header("Scenes List")]
+    public SceneData[] Scenes;
+
+    void OnEnable()
+    {
+        for (int i = 0; i < Scenes.Length; i++)
+        {
+            if (Scenes[i].sceneDisplayName.Equals(""))
+                Scenes[i].sceneDisplayName = Scenes[i].scene.name;
+                
+            Scenes[i].scenePath = AssetDatabase.GetAssetPath(Scenes[i].scene.GetInstanceID());
+        }
+        //Debug.Log(Scenes.Length);
+    }
+}
+
+[System.Serializable]
+public struct SceneData
+{
+    public SceneAsset scene;
+    public string sceneDisplayName;
+    public string scenePath;
+}
