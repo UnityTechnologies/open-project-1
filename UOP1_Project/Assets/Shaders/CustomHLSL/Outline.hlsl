3abf985a6f75e3a002e273b9c330d24d554a762e
treivize <ypuyhaubert@gmail.com>
2020-12-11 11:45:12 +0100

    Outline improvements - New layers

--- UOP1_Project/Assets/Shaders/CustomHLSL/Outline.hlsl
+++ UOP1_Project/Assets/Shaders/CustomHLSL/Outline.hlsl
@@ -10,6 +10,9 @@ float4 _CameraDepthTexture_TexelSize;
 
 TEXTURE2D(_CameraDepthNormalsTexture);
 SAMPLER(sampler_CameraDepthNormalsTexture);
+
+TEXTURE2D(_CameraOutlineTicknessTexture);
+SAMPLER(sampler_CameraOutlineTicknessTexture);
  
 float3 DecodeNormal(float4 enc)
 {
@@ -22,39 +25,86 @@ float3 DecodeNormal(float4 enc)
     return n;
 }
 
-void OutlineObject_float(float2 UV, float OutlineThickness, float DepthSensitivity, float NormalsSensitivity, out float Out)
-{
-    float halfScaleFloor = floor(OutlineThickness * 0.5);
-    float halfScaleCeil = ceil(OutlineThickness * 0.5);
-    
-    float2 uvSamples[4];
-    float depthSamples[4];
-    float3 normalSamples[4];
+// This method checks where the sampling pixels are regarding to the outlined object mask renderer pass
+// If all the points are out of the mask (black pixels) then the funtion return 0 (Force no outline)
+// If at least one pixel is black and another is white the function return (Force no outline)
+// If all the pixels are white (inside an area covered by outlined objects). Return 0.5 half outline size.
+float getTicknessBiasing(float2 UV, float OutlineThickness) {
+	float halfScaleFloor = floor(OutlineThickness * 0.5);
+	float halfScaleCeil = ceil(OutlineThickness * 0.5);
 
-    uvSamples[0] = UV - float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleFloor;
-    uvSamples[1] = UV + float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleCeil;
-    uvSamples[2] = UV + float2(_CameraDepthTexture_TexelSize.x * halfScaleCeil, -_CameraDepthTexture_TexelSize.y * halfScaleFloor);
-    uvSamples[3] = UV + float2(-_CameraDepthTexture_TexelSize.x * halfScaleFloor, _CameraDepthTexture_TexelSize.y * halfScaleCeil);
+	float2 uvSamples[4];
+	float allSamplesIn = 1.0;
+	float atLeastOneSampleIn = 0.0;
 
-    for(int i = 0; i < 4 ; i++)
-    {
-        depthSamples[i] = SAMPLE_TEXTURE2D(_CameraDepthTexture, sampler_CameraDepthTexture, uvSamples[i]).r;
-        normalSamples[i] = DecodeNormal(SAMPLE_TEXTURE2D(_CameraDepthNormalsTexture, sampler_CameraDepthNormalsTexture, uvSamples[i]));
-    }
-
-    // Depth
-    float depthFiniteDifference0 = depthSamples[1] - depthSamples[0];
-    float depthFiniteDifference1 = depthSamples[3] - depthSamples[2];
-    float edgeDepth = sqrt(pow(depthFiniteDifference0, 2) + pow(depthFiniteDifference1, 2)) * 100;
-    float depthThreshold = (1/DepthSensitivity) * depthSamples[0];
-    edgeDepth = edgeDepth > depthThreshold ? 1 : 0;
-
-    // Normals
-    float3 normalFiniteDifference0 = normalSamples[1] - normalSamples[0];
-    float3 normalFiniteDifference1 = normalSamples[3] - normalSamples[2];
-    float edgeNormal = sqrt(dot(normalFiniteDifference0, normalFiniteDifference0) + dot(normalFiniteDifference1, normalFiniteDifference1));
-    edgeNormal = edgeNormal > (1/NormalsSensitivity) ? 1 : 0;
-
-    float edge = max(edgeDepth, edgeNormal);
-    Out = edge;
+	uvSamples[0] = UV - float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleFloor;
+	uvSamples[1] = UV + float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleCeil;
+	uvSamples[2] = UV + float2(_CameraDepthTexture_TexelSize.x * halfScaleCeil, -_CameraDepthTexture_TexelSize.y * halfScaleFloor);
+	uvSamples[3] = UV + float2(-_CameraDepthTexture_TexelSize.x * halfScaleFloor, _CameraDepthTexture_TexelSize.y * halfScaleCeil);
+	
+	float maskSample;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		maskSample = SAMPLE_TEXTURE2D(_CameraOutlineTicknessTexture, sampler_CameraOutlineTicknessTexture, uvSamples[i]).r;
+		allSamplesIn = allSamplesIn * maskSample;
+		atLeastOneSampleIn = atLeastOneSampleIn + maskSample;
+	}
+	return atLeastOneSampleIn == 0 ? 0.0 : (1.0 - 0.5 * allSamplesIn);
 }
+
+void OutlineObject_float(float2 UV, float OutlineThickness, float DepthSensitivity, float NormalsSensitivity, float DepthNormalSensitivity, float DepthNormalThresholdScale, float3 viewDir, out float Out)
+{
+	float biasedThickness = getTicknessBiasing(UV, OutlineThickness);
+	if (biasedThickness == 0.0) //The considered pixel is far from an outlined object so no need to draw outline.
+	{
+		Out = 0.0;
+	}
+	else if (biasedThickness == 1.0) // The considered pixel is on the border between outlined and not outlined object. Always draw an outline.
+	{
+		Out = 1.0;
+	}
+	else // Inner outline and outline between overlapping outlined objects. Apply the depth/normal technique with half outline thickness to balance that all outline width will be drawn 
+	{
+		float halfScaleFloor = floor(biasedThickness * OutlineThickness * 0.5);
+		float halfScaleCeil = ceil(biasedThickness * OutlineThickness * 0.5);
+
+		float2 uvSamples[5];
+		float depthSamples[5];
+		float3 normalSamples[5];
+
+		uvSamples[0] = UV;
+		uvSamples[1] = UV - float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleFloor;
+		uvSamples[2] = UV + float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleCeil;
+		uvSamples[3] = UV + float2(_CameraDepthTexture_TexelSize.x * halfScaleCeil, -_CameraDepthTexture_TexelSize.y * halfScaleFloor);
+		uvSamples[4] = UV + float2(-_CameraDepthTexture_TexelSize.x * halfScaleFloor, _CameraDepthTexture_TexelSize.y * halfScaleCeil);
+
+		for (int i = 0; i < 5; i++)
+		{
+			depthSamples[i] = SAMPLE_TEXTURE2D(_CameraDepthTexture, sampler_CameraDepthTexture, uvSamples[i]).r;
+			normalSamples[i] = DecodeNormal(SAMPLE_TEXTURE2D(_CameraDepthNormalsTexture, sampler_CameraDepthNormalsTexture, uvSamples[i]));
+		}
+
+		// Depth
+		float depthFiniteDifference0 = depthSamples[2] - depthSamples[1];
+		float depthFiniteDifference1 = depthSamples[4] - depthSamples[3];
+		float edgeDepth = sqrt(pow(depthFiniteDifference0, 2) + pow(depthFiniteDifference1, 2)) * 100;
+
+		// Thresholding with view direction. Balance normal difference based on the camera direction for flat surface viewed from a grazing angle
+		float NdotV = 1 - dot(normalSamples[0], viewDir);
+		float normalThreshold01 = saturate((NdotV - DepthNormalSensitivity) / (1 - DepthNormalSensitivity));
+		float normalThreshold = normalThreshold01 * DepthNormalThresholdScale + 1;
+		float depthThreshold = (biasedThickness / DepthSensitivity) * depthSamples[0] * normalThreshold;
+		edgeDepth = edgeDepth > depthThreshold ? 1 : 0;
+
+		// Normals
+		float3 normalFiniteDifference0 = normalSamples[2] - normalSamples[1];
+		float3 normalFiniteDifference1 = normalSamples[4] - normalSamples[3];
+		float edgeNormal = sqrt(dot(normalFiniteDifference0, normalFiniteDifference0) + dot(normalFiniteDifference1, normalFiniteDifference1));
+		edgeNormal = edgeNormal > (1 / NormalsSensitivity) ? 1 : 0;
+
+		float edge = max(edgeDepth, edgeNormal);
+		Out = edge;
+	}
+}
+
---
+++ UOP1_Project/Assets/Shaders/ScriptableRenderFeatures/OutlineTicknessFeature.cs
@@ -0,0 +1,117 @@
+﻿// This code is an adaptation of the open-source work by Alexander Ameye
+// From a tutorial originally posted here:
+// https://alexanderameye.github.io/outlineshader
+// Code also available on his Gist account
+// https://gist.github.com/AlexanderAmeye
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Profiling;
+
+public class OutlineTicknessFeature : ScriptableRendererFeature
+{
+    class OutlineTicknessPass : ScriptableRenderPass
+    {
+        int kDepthBufferBits = 32;
+        private RenderTargetHandle outlineTicknessAttachmentHandle { get; set; }
+        internal RenderTextureDescriptor descriptor { get; private set; }
+
+        private Material outlineTicknessMaterial = null;
+        private FilteringSettings m_FilteringSettings;
+        string m_ProfilerTag = "OutlineTickness Prepass";
+        ShaderTagId m_ShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+        public OutlineTicknessPass(RenderQueueRange renderQueueRange, LayerMask layerMask, Material material)
+        {
+            m_FilteringSettings = new FilteringSettings(renderQueueRange, layerMask);
+			outlineTicknessMaterial = material;
+        }
+
+        public void Setup(RenderTextureDescriptor baseDescriptor, RenderTargetHandle outlineTicknessAttachmentHandle)
+        {
+            this.outlineTicknessAttachmentHandle = outlineTicknessAttachmentHandle;
+            baseDescriptor.colorFormat = RenderTextureFormat.ARGB32;
+            baseDescriptor.depthBufferBits = kDepthBufferBits;
+            descriptor = baseDescriptor;
+        }
+
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            cmd.GetTemporaryRT(outlineTicknessAttachmentHandle.id, descriptor, FilterMode.Point);
+            ConfigureTarget(outlineTicknessAttachmentHandle.Identifier());
+            ConfigureClear(ClearFlag.All, Color.black);
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+			
+            using (new ProfilingScope(cmd, new ProfilingSampler(m_ProfilerTag)))
+            {
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+
+                var sortFlags = renderingData.cameraData.defaultOpaqueSortFlags;
+                var drawSettings = CreateDrawingSettings(m_ShaderTagId, ref renderingData, sortFlags);
+                drawSettings.perObjectData = PerObjectData.None;
+
+
+                ref CameraData cameraData = ref renderingData.cameraData;
+                Camera camera = cameraData.camera;
+                if (cameraData.isStereoEnabled)
+                    context.StartMultiEye(camera);
+
+				drawSettings.overrideMaterial = outlineTicknessMaterial;
+
+				context.DrawRenderers(renderingData.cullResults, ref drawSettings,
+                    ref m_FilteringSettings);
+
+                cmd.SetGlobalTexture("_CameraOutlineTicknessTexture", outlineTicknessAttachmentHandle.id);
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            if (outlineTicknessAttachmentHandle != RenderTargetHandle.CameraTarget)
+            {
+                cmd.ReleaseTemporaryRT(outlineTicknessAttachmentHandle.id);
+				outlineTicknessAttachmentHandle = RenderTargetHandle.CameraTarget;
+            }
+        }
+    }
+
+	OutlineTicknessPass outlineTicknessPass;
+    RenderTargetHandle outlineTicknessTexture;
+	public Material outlineTicknessMaterial;
+	public LayerMask outlineTicknessLayerMask;
+
+	public override void Create()
+    {
+		outlineTicknessPass = new OutlineTicknessPass(RenderQueueRange.opaque, outlineTicknessLayerMask, outlineTicknessMaterial);
+		outlineTicknessPass.renderPassEvent = RenderPassEvent.AfterRenderingPrePasses;
+		outlineTicknessTexture.Init("_CameraOutlineTicknessTexture");
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+		outlineTicknessPass.Setup(renderingData.cameraData.cameraTargetDescriptor, outlineTicknessTexture);
+        renderer.EnqueuePass(outlineTicknessPass);
+    }
+}
+
