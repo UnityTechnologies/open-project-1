7820e00bff4cfc9e084a52d9dd9308834f8240c1
Ciro Continisio <ciro@unity3d.com>
2020-12-18 18:47:24 +0100

    Added Run and Faint animation for PigChef, implemented Run as BlendTree

---
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Actions/AnimatorMoveSpeedActionSO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+using Moment = UOP1.StateMachine.StateAction.SpecificMoment;
+
+/// <summary>
+/// Flexible StateActionSO for the StateMachine which allows to set any parameter on the Animator, in any moment of the state (OnStateEnter, OnStateExit, or each OnUpdate).
+/// </summary>
+[CreateAssetMenu(fileName = "AnimatorMoveSpeedAction", menuName = "State Machines/Actions/Set Animator Move Speed")]
+public class AnimatorMoveSpeedActionSO : StateActionSO
+{
+	public string parameterName = default;
+
+	protected override StateAction CreateAction() => new AnimatorMoveSpeedAction(Animator.StringToHash(parameterName));
+}
+
+public class AnimatorMoveSpeedAction : StateAction
+{
+	//Component references
+	private Animator _animator;
+	private Protagonist _protagonist;
+
+	private AnimatorParameterActionSO _originSO => (AnimatorParameterActionSO)base.OriginSO; // The SO this StateAction spawned from
+	private int _parameterHash;
+
+	public AnimatorMoveSpeedAction(int parameterHash)
+	{
+		_parameterHash = parameterHash;
+	}
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_animator = stateMachine.GetComponent<Animator>();
+		_protagonist = stateMachine.GetComponent<Protagonist>();
+	}
+
+	public override void OnUpdate()
+	{
+		//TODO: do we like that we're using the magnitude here, per frame? Can this be done in a smarter way?
+		float normalisedSpeed = _protagonist.movementInput.magnitude;
+		_animator.SetFloat(_parameterHash, normalisedSpeed);
+	}
+}
