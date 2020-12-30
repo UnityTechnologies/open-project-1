a2955a86b78714a8c72b9ac99b6a7b3dbad39d21
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-12-30 13:56:02 +0530

    Added TestScript for explanation of functionality

--- UOP1_Project/Assets/Scripts/SaveSystem/Save.cs
+++ UOP1_Project/Assets/Scripts/SaveSystem/Save.cs
@@ -5,5 +5,12 @@
 /// </summary>
 [System.Serializable]
 public class Save {
-	
+	// This is test data, written accodring to TestScript.cs class
+	// This will change according to whatever data that needs to be stored
+
+	// The variables need to be public, else we would have to write trivial getter/setter functions.
+
+	public int _testInteger = default;
+	public float _testFloat = default;
+	public bool _testBool = default;
 }
