10816cef5c01c309ad721dfa277ebea60fc1e7e4
Amel Negra <amel.negra@unity3d.com>
2021-01-08 10:35:26 +0100

    Added loadingScreenManager to listen to UI toggle events

---
+++ UOP1_Project/Assets/Scripts/SceneManagement/LoadingScreenManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class LoadingScreenManager : MonoBehaviour
+{
+	[Header("Loading screen Event")]
+	//The loading screen event we are listening to
+	[SerializeField] private BoolEventChannelSO _ToggleLoadingScreen = default;
+
+	private void OnEnable()
+	{
+		if (_ToggleLoadingScreen != null)
+		{
+			_ToggleLoadingScreen.OnEventRaised += ToggleLoadingScreen;
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (_ToggleLoadingScreen != null)
+		{
+			_ToggleLoadingScreen.OnEventRaised += ToggleLoadingScreen;
+		}		
+	}
+
+	private void ToggleLoadingScreen(bool state)
+	{
+		gameObject.SetActive(state);
+	}
+
+}
