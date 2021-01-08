ade45e548326f38b1acee816d08f3b0faa96469f
Amel Negra <amel.negra@unity3d.com>
2021-01-08 10:34:47 +0100

    EditorInitialisationLoader now works with menu and locations

--- UOP1_Project/Assets/Scripts/SceneManagement/EditorInitialisationLoader.cs
+++ UOP1_Project/Assets/Scripts/SceneManagement/EditorInitialisationLoader.cs
@@ -2,27 +2,32 @@
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// This script loads the initial Scene, to allow to start the game from any gameplay Scene
+/// This script loads the persistent managers and gameplay Scenes, to allow to start the game from any gameplay Scene
+/// It can also be used for menu scene by just adding the persistent managers scene on the inspector
 /// </summary>
 public class EditorInitialisationLoader : MonoBehaviour
 {
 #if UNITY_EDITOR
-	public GameSceneSO initializationScene;
+	public GameSceneSO[] persistentScenes;
 	public int targetFramerate = 0; // For debugging purposes
 
-	private void Awake()
+	private void Start()
 	{
 		Application.targetFrameRate = targetFramerate; // For debugging purposes
 
 		for (int i = 0; i < SceneManager.sceneCount; ++i)
 		{
 			Scene scene = SceneManager.GetSceneAt(i);
-			if (scene.path == initializationScene.scenePath)
-			{
-				return;
-			}
+			for (int j = 0; j < persistentScenes.Length; ++j)
+				if (scene.path == persistentScenes[j].scenePath)
+				{
+					return;
+				}
+				else
+				{
+					SceneManager.LoadSceneAsync(persistentScenes[j].scenePath, LoadSceneMode.Additive);
+				}
 		}
-		SceneManager.LoadSceneAsync(initializationScene.scenePath, LoadSceneMode.Additive);
 	}
 #endif
 }
