91d9fe4778f315e86f5c7c8d767bdbe3ad64b261
Amel Negra <amel.negra@unity3d.com>
2021-01-08 12:53:54 +0100

    Added method to broadcast that scene is ready (Press play from any sc)

---
+++ UOP1_Project/Assets/SceneReadyBroadcaster.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class manages the case we are in editor and we want to press play from any scene
+/// It takes care of telling the SpawnSystem that the scene is ready since we pressed play from it
+/// and it is therefore already loaded
+/// </summary> 
+public class SceneReadyBroadcaster : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+	[Header("On Editor Initializer event")]
+	[SerializeField] private VoidEventChannelSO _OnEditorInitializer = default;
+
+
+	[Header("Broadcasting on")]
+	[SerializeField] private VoidEventChannelSO _OnSceneReady = default;
+
+	private void OnEnable()
+	{
+		if (_OnEditorInitializer != null)
+		{
+			_OnEditorInitializer.OnEventRaised += InformSceneIsReady;
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (_OnEditorInitializer != null)
+		{
+			_OnEditorInitializer.OnEventRaised -= InformSceneIsReady;
+		}
+	}
+
+	void InformSceneIsReady()
+	{
+		_OnSceneReady.RaiseEvent();
+	}
+#endif
+}
