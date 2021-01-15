1a66aca2000221bb63dcf75fb9526036b8048b73
IroncladLandship <jack.huffstutler@gmail.com>
2021-01-14 22:14:22 -0600

    UI Fade in and out code and implementation
    This implements the UI fader to black card using events and scriptable objects. To use, add a FadeChannelSO to the relevant script, assign the scriptable object, the call either Fade, FadeIn, or FadeOut. Has parameters for duration and target color. This is usable anywhere and at any time, but contains no safeguards besides preventing multiple fades from happening at the same time.
    
    Note: This modifies the PersistentManagers scene and adds an Image UI element that it depends on.

---
+++ UOP1_Project/Assets/Scripts/Events/ScriptableObjects/UI/FadeChannelSO.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Events/UI/Fade Channel")]
+public class FadeChannelSO : ScriptableObject
+{
+	public UnityAction<bool, float, Color> OnEventRaised;
+	/// <summary>
+	/// Generic fade function. Communicates with <seealso cref="FadeManager.cs"/>.
+	/// </summary>
+	/// <param name="fadeIn">If true, the rectangle fades in. If false, the rectangle fades out.</param>
+	/// <param name="duration">How long it takes to the image to fade in/out.</param>
+	/// <param name="color">Target color for the image to reach. Disregarded when fading out.</param>
+	public void Fade(bool fadeIn, float duration, Color color)
+	{
+		if (OnEventRaised != null)
+			OnEventRaised.Invoke(fadeIn, duration, color);
+	}
+
+	/// <summary>
+	/// Generic fade function. Communicates with <seealso cref="FadeManager.cs"/>.
+	/// </summary>
+	/// <param name="fadeIn">If true, the rectangle fades in. If false, the rectangle fades out.</param>
+	/// <param name="duration">How long it takes to the image to fade in/out.</param>
+	public void Fade(bool fadeIn, float duration)
+	{
+		if (OnEventRaised != null)
+			OnEventRaised.Invoke(fadeIn, duration, new Color(0, 0, 0, 1));
+	}
+
+	/// <summary>
+	/// Fade helper function to simplify usage. Fades in the rectangle.
+	/// </summary>
+	/// <param name="duration">How long it takes to the image to fade in.</param>
+	/// <param name="color">Target color for the image to reach.</param>
+	public void FadeIn(float duration, Color color)
+	{
+		Fade(true, duration, color);
+	}
+	/// <summary>
+	/// Fade helper function to simplify usage. Fades in the rectangle.
+	/// </summary>
+	/// <param name="duration">How long it takes to the image to fade in.</param>
+	public void FadeIn(float duration)
+	{
+		Fade(true, duration, new Color(0, 0, 0, 1));
+	}
+	/// <summary>
+	/// Fade helper function to simplify usage. Fades out the rectangle.
+	/// </summary>
+	/// <param name="duration">How long it takes to the image to fade out.</param>
+	public void FadeOut(float duration)
+	{
+		Fade(false, duration, new Color(0, 0, 0, 1));
+	}
+}
