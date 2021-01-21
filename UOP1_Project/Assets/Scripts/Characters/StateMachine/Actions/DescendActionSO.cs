eedc80c0b51b5aa47c6bde5cf229569485bd14d6
David Lischinsky <david.lischinsky@outlook.com>
2021-01-21 13:41:50 -0300

    Update PigChef TransitionTable, Jumping and Sliding mechanics (#310)
    * Update PigChef_TransitionTable.asset
    
    Changes:
    - From Idle
      - To Sliding
        - ~~IsActuallyMoving~~
      - To JumpAscending
         - ~~IsSliding~~
    - From Walking
      - **To Sliding (moved above To JumpAscending)**
      - To JumpAscending
        - ~~IsSliding~~
    
    * Reordered Walking and Idle states transitions.
    
    - To Sliding
    - To JumpAscending
    - To JumpDescending
    - To Attack
    - To PickUp
    - To Talk
    - To Walking/Idle
    
    * Update PigChef.controller
    
    Updated PigChef Animator controller to reflect the changes to the TransitionTable:
    - Added transition from Walking to Collect on PickUp trigger.
    - Added transition from Walking to Talking when IsTalking is True.
    
    * Added MidSliding State
    
    The MidSliding State is added to prevent the state machine from going back and forth between Sliding and JumpDescending when not needed.
    
    * Major changes to player sliding and aerial movement, bug fixes, and adjustments to the PigChef
    
    - Added `AerialMovementActionSO.cs`. This Action handles horizontal movement while in the air, keeping momentum, simulating air resistance, and accelerating towards the desired speed instead of using a constant value. This makes jumping feel more natural and realistic. Configurable via the Speed and Acceleration fields on the SO, and air resistance is a constant in `Protagonist.cs`.
    - Replaced `MoveInTheAir.asset` with `AerialMovement.asset` in **JumpAscending** and **JumpDescending** States.
    - Rewrote `SlideActionSO.cs` to improve the sliding mechanic. The velocity and direction during the slide are now based on the previous velocity and the action of gravity. Just as the changes to jumpin, this makes it feel way more natural and realistic.
    - Removed `GravitySliding.asset` since all movement components (x, y, z) are now calculated in `SlideActionSO.cs`.
    - `ApplyMovementVectorActionSO.cs` now sets `Protagonist.movementVector` to `CharacterController.velocity` calling `Move()`, this way the next frame any script can get the actual velocity the character is moving. This was needed for both the new aerial and sliding mechanics.
    - Removed from the code the lines where `Protagonist.movementVector` was randomly being set to `Vector3.zero` (e.g.: `IsMovingConditionSO.cs`).
    - Created `StopMovementActionSO.cs`. An Action to clear `Protagonist.movementVector` at the `SpecificMoment`.
    - Created `StopMovement_OnEnter.asset` and added it to the **Idle** State.
    - Moved constant values to `Protagonist.cs` so they can be accessed from any script, and duplicates avoided (we already had two GRAVITY_MULTIPLIER and I needed it as well for the new Slide).
    - Changed all the `[HideInInspector]` to `[NonSerialized]`  (`[HideInInspector]` still serializes, which it's why you might have noticed sometimes the character would attack as soon as entering play mode).
    
    * [Bot] Automated dotnet-format update

--- UOP1_Project/Assets/Scripts/Characters/StateMachine/Actions/DescendActionSO.cs
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Actions/DescendActionSO.cs
@@ -11,9 +11,6 @@ public class DescendAction : StateAction
 	private Protagonist _protagonistScript;
 
 	private float _verticalMovement;
-	private const float GRAVITY_MULTIPLIER = 5f;
-	private const float MAX_FALL_SPEED = -50f;
-	private const float MAX_RISE_SPEED = 100f;
 
 	public override void Awake(StateMachine stateMachine)
 	{
@@ -31,11 +28,11 @@ public class DescendAction : StateAction
 
 	public override void OnUpdate()
 	{
-		_verticalMovement += Physics.gravity.y * GRAVITY_MULTIPLIER * Time.deltaTime;
+		_verticalMovement += Physics.gravity.y * Protagonist.GRAVITY_MULTIPLIER * Time.deltaTime;
 		//Note that even if it's added, the above value is negative due to Physics.gravity.y
 
 		//Cap the maximum so the player doesn't reach incredible speeds when freefalling from high positions
-		_verticalMovement = Mathf.Clamp(_verticalMovement, MAX_FALL_SPEED, MAX_RISE_SPEED);
+		_verticalMovement = Mathf.Clamp(_verticalMovement, Protagonist.MAX_FALL_SPEED, Protagonist.MAX_RISE_SPEED);
 
 		_protagonistScript.movementVector.y = _verticalMovement;
 	}
