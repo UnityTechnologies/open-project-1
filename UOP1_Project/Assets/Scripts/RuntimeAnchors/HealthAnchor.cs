922b9b15dd8767f7048e7301cd36b3b835086ff0
treivize <ypuyhaubert@gmail.com>
2021-01-22 17:34:10 +0100

    Player Health anchor SO

---
+++ UOP1_Project/Assets/Scripts/RuntimeAnchors/HealthAnchor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Runtime Anchors/Health")]
+public class HealthAnchor : RuntimeAnchorBase
+{
+	[SerializeField]
+	private int _MaxHealth;
+
+	private int _currenHealth;
+
+	public int CurrentHealth { get => _currenHealth; }
+
+	public void FillHealth()
+	{
+		_currenHealth = _MaxHealth;
+	}
+
+	public void DecreaseHealth(int damage)
+	{
+		_currenHealth -= damage;
+	}
+
+	public bool isDead()
+	{
+		return _currenHealth <= 0;
+	}
+}
