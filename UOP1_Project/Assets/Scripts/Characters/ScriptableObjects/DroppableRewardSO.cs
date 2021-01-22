f40583a20c6592e29a5530990196a3bc28d673e5
treivize <ypuyhaubert@gmail.com>
2021-01-22 22:45:36 +0100

    Fix dropping algo

--- UOP1_Project/Assets/Scripts/Characters/ScriptableObjects/DroppableRewardSO.cs
+++ UOP1_Project/Assets/Scripts/Characters/ScriptableObjects/DroppableRewardSO.cs
@@ -11,47 +11,49 @@ public class DroppableRewardSO : ScriptableObject
 
 	[Tooltip("The list of drop goup that can be dropped by this critter when killed")]
 	[SerializeField]
-	private List<DropGroup> _dropItems = new List<DropGroup>();
+	private List<DropGroup> _dropGroups = new List<DropGroup>();
 
 	public void DropReward(Vector3 postion)
 	{
 		// Drop items
-		for (int i = 0; i < _dropItems.Count; i++)
+		foreach (DropGroup dropGroup in _dropGroups)
 		{
 			float randValue = Random.value;
-			DropGroup dropGroup = _dropItems[i];
 			if (dropGroup.DropRate >= randValue)
 			{
-				float dropDice = Random.value;
-				float _currentRate = 0.0f;
-
-				Item item = null;
-				GameObject itemPrefab = null;
-
-				foreach (DropItem dropItem in dropGroup.Drops)
-				{
-					_currentRate += dropItem.ItemDropRate;
-					if (_currentRate >= dropDice)
-					{
-						item =  dropItem.Item;
-						itemPrefab = dropItem.CollectibleItemPrefab;
-					}
-				}
-
-				float randAngle = Random.value * Mathf.PI * 2;
-
-				GameObject collectibleItem = GameObject.Instantiate(itemPrefab,
-					postion + itemPrefab.transform.localPosition +
-					_scatteringDistance * (Mathf.Cos(randAngle) * Vector3.forward + Mathf.Sin(randAngle) * Vector3.right),
-					Quaternion.identity);
-				collectibleItem.GetComponent<CollectibleItem>().CurrentItem = item;
+				DropOneReward(dropGroup, postion);
 			}
 			else
 			{
 				break;
 			}
-
-			
 		}
+	}
+
+	private void DropOneReward(DropGroup dropGroup, Vector3 postion)
+	{
+		float dropDice = Random.value;
+		float _currentRate = 0.0f;
+
+		Item item = null;
+		GameObject itemPrefab = null;
+
+		foreach (DropItem dropItem in dropGroup.Drops)
+		{
+			_currentRate += dropItem.ItemDropRate;
+			if (_currentRate >= dropDice)
+			{
+				item = dropItem.Item;
+				itemPrefab = dropItem.CollectibleItemPrefab;
+				break;
+			}
+		}
+
+		float randAngle = Random.value * Mathf.PI * 2;
+		GameObject collectibleItem = GameObject.Instantiate(itemPrefab,
+			postion + itemPrefab.transform.localPosition +
+			_scatteringDistance * (Mathf.Cos(randAngle) * Vector3.forward + Mathf.Sin(randAngle) * Vector3.right),
+			Quaternion.identity);
+		collectibleItem.GetComponent<CollectibleItem>().CurrentItem = item;
 	}
 }
