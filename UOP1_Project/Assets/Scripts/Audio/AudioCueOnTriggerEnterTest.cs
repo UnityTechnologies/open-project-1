35c55b0a9e861e190dbc8c47e6744dcdc45f4d4a
Marc-Olivier <islandx424@gmail.com>
2021-01-28 16:42:21 -0500

    Add a Stop and Finish option for the AudioManager

---
+++ UOP1_Project/Assets/Scripts/Audio/AudioCueOnTriggerEnterTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Audio
+{
+	/// <summary>
+	/// Class to activate AudioCues when a GameObject (i.e. the Player) enters the trigger Collider on this same GameObject.
+	/// This component is mostly used for testing purposes.
+	/// </summary>
+	[RequireComponent(typeof(AudioCue))]
+	public class AudioCueOnTriggerEnterTest : MonoBehaviour
+	{
+		[SerializeField] private bool _isInstantStop = true;
+		[SerializeField] private string _tagToDetect = "Player";
+
+		private AudioCue audioCue;
+
+		private void Awake()
+		{
+			audioCue = GetComponent<AudioCue>();
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.CompareTag(_tagToDetect))
+				audioCue.PlayAudioCue();
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if (other.gameObject.CompareTag(_tagToDetect))
+			{
+				if (_isInstantStop)
+					audioCue.StopAudioCue();
+				else
+					audioCue.FinishAudioCue();
+			}
+		}
+	}
+}
