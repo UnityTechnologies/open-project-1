eadf9b4c8b03ec470980369943065f5583cedfdd
Yohann Puyhaubert <ypuyhaubert@gmail.com>
2021-02-08 09:44:16 +0100

    Added slash effect when player is triggering an attack

---
+++ UOP1_Project/Assets/Scripts/Characters/AttackEffectController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEffectController : MonoBehaviour
+{
+	[SerializeField]
+	private ParticleSystem _slashEffect;
+	[SerializeField]
+	private ParticleSystem _reverseSlashEffect;
+
+	private void Start()
+	{
+		_slashEffect.Stop();
+		_reverseSlashEffect.Stop();
+	}
+
+	public void PlaySlash()
+	{
+		_slashEffect.Play();
+	}
+
+	public void PlayReverseSlash()
+	{
+		_reverseSlashEffect.Play();
+	}
+
+}
