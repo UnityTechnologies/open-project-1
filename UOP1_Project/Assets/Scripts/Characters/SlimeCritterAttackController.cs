8a76057ff3cbd525a94380aeb61bee572cc3c599
Yohann Puyhaubert <ypuyhaubert@gmail.com>
2021-02-12 11:21:24 +0100

    Prevent Slime Critter propel attack to overlap with the player

--- UOP1_Project/Assets/Scripts/Characters/SlimeCritterAttackController.cs
+++ UOP1_Project/Assets/Scripts/Characters/SlimeCritterAttackController.cs
@@ -1,28 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SlimeCritterAttackController : MonoBehaviour
 {
-	// Reference of the player transform to compute the propel target position
-	[SerializeField]
-	private TransformAnchor _playerTransform;
-
-	// Propel factor is the proportion of the distance between the critter and the player crossed by the critter during the propel animation
-	[SerializeField]
-	private float _propelFactor = 1.0f;
-
 	// Duration of the propel section of the animation.
 	[SerializeField]
 	private float _propelDuration = 0.2f;
 
+	[SerializeField]
+	private float _distanceFromTarget = 0.7f;
+
 	private float _innerTime = 0.0f;
 	private Vector3 _propelTargetVector = default;
+
+	private Critter _critter;
+
+	private void Awake()
+	{
+		_critter = GetComponent<Critter>();
+	}
 
 	// When the attack starts, the position targeted by the attack is determined and is not changed afterward
 	public void SetAttackTarget()
 	{
-		_propelTargetVector = (_playerTransform.Transform.position - transform.position) * _propelFactor / _propelDuration;
+		_propelTargetVector = Vector3.zero;
+		if (_critter.currentTarget != null)
+		{
+			Vector3 dir = _critter.currentTarget.gameObject.transform.position - transform.position;
+			// Target a position 0.3f far from player center (approximately the player model radius) to avoid entering in the player. 
+			_propelTargetVector = (dir - dir.normalized * _distanceFromTarget) / _propelDuration;
+
+			//Prevent critter to propel backward if he is too close to the player.
+			if (Vector3.Dot(dir, _propelTargetVector) < 0)
+			{
+				_propelTargetVector = Vector3.zero;
+			}
+		}
 	}
 
 	// Trigger the propel movement during the attack
