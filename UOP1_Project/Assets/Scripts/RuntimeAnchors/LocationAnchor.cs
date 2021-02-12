3795d656776ac010647e736f5146d563cd784572
HarshNarayanJha <pawanjha2802.pj@gmail.com>
2021-02-12 18:06:07 +0530

    Created Location Anchor To save location

---
+++ UOP1_Project/Assets/Scripts/RuntimeAnchors/LocationAnchor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New LocationAnchor", menuName = "Runtime Anchors/Location")]
+public class LocationAnchor : RuntimeAnchorBase
+{
+	[HideInInspector] public bool isSet = false; // Any script can check if the transform is null before using it, by just checking this bool
+
+	private LocationSO _Location;
+	public LocationSO Location
+	{
+		get { return _Location; }
+		set
+		{
+			_Location = value;
+			isSet = _Location != null;
+		}
+	}
+
+	public void OnDisable()
+	{
+		_Location = null;
+		isSet = false;
+	}
+}
