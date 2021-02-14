0aacbc28c32276703ec486776305428427f9948e
vjs22334 <vjs122334@gmail.com>
2021-02-14 10:25:31 +0530

    added a way to use collider volume to swap camera

--- UOP1_Project/Assets/Scripts/CameraSwapTrigger.cs
+++ UOP1_Project/Assets/Scripts/CameraSwapTrigger.cs
@@ -6,15 +6,27 @@ using Cinemachine;
 public class CameraSwapTrigger : MonoBehaviour
 {
     public CinemachineVirtualCamera cameraToSwap;
+    [Tooltip("if this is set, the camera will be swapped when the player enters the volume of the trigger and go back to default when he exist the volume. Otherwise the camera swap will take place based on whether the player exits along or opposye to local z axis")]
+    public bool UseVolume = false;
     
     [Header("Asset references")]
     [SerializeField] private VcamEventChannelSO VcamEventChannel = default;
 
-    BoxCollider boxCollider;
+    Collider Collider;
 
     void Start()
     {
-        boxCollider = GetComponent<BoxCollider>();
+        Collider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            if(UseVolume){
+                //player entered the volume so switch to our camera
+                VcamEventChannel.OnEventRaised(cameraToSwap);
+            }
+        }
     }
 
 
@@ -23,21 +35,26 @@ public class CameraSwapTrigger : MonoBehaviour
         // check if it is the player
         if(other.CompareTag("Player")){
             
-            //check which side the player exited. we consider only the z axis so the enter and exit faces should be along the local z axis.
-            
-            Vector3 playerPosInLocalCoordinates = transform.InverseTransformPoint(other.transform.position);
-            
-            if(boxCollider.center.z > playerPosInLocalCoordinates.z){
-                
-                //player exited on -ve z side which means we switch to the default camera
+            if(UseVolume){
+                //player exited the volume so switch back to default cam
                 VcamEventChannel.OnEventRaised(null);
-
             }
             else{
+                //check which side the player exited. we consider only the z axis so the enter and exit faces should be along the local z axis.
+                Vector3 playerPosInLocalCoordinates = transform.InverseTransformPoint(other.transform.position);
+                
+                if(Collider.bounds.center.z > playerPosInLocalCoordinates.z){
+                    
+                    //player exited on -ve z side which means we switch to the default camera
+                    VcamEventChannel.OnEventRaised(null);
 
-                //player exited on +ve z side which means we switch to our camera
-                VcamEventChannel.OnEventRaised(cameraToSwap);
-            
+                }
+                else{
+
+                    //player exited on +ve z side which means we switch to our camera
+                    VcamEventChannel.OnEventRaised(cameraToSwap);
+                
+                }
             }
         }
    }
