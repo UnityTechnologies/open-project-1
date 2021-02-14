2084c766669ab4a175ba4ae0ba302313eba36b88
Yohann Puyhaubert <ypuyhaubert@gmail.com>
2021-02-14 15:47:53 +0100

    Added pathway NPC movement strategy

---
+++ UOP1_Project/Assets/Scripts/Characters/Config/PathwayConfigSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PathwayConfig", menuName = "EntityConfig/Pathway Config")]
+public class PathwayConfigSO : NPCMovementConfigSO
+{
+	[Tooltip("Pathway waypoints")]
+	[SerializeField] private List<Vector3> _waypoints;
+
+	public List<Vector3> Waypoints => _waypoints;
+
+}
---
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Actions/MovementActions/RoamingMovementAction.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamingMovementAction : NPCMovementAction
+{
+	private NavMeshAgent _agent;
+	private bool _isActiveAgent;
+	private Vector3 _startPosition;
+
+	private float _roamingSpeed;
+	private float _roamingDistance;
+
+	private Vector3 _roamingTargetPosition;
+
+	public RoamingMovementAction(
+		RoamingAroundCenterConfigSO config, NavMeshAgent agent, Vector3 startPosition)
+	{
+		_agent = agent;
+		_isActiveAgent = _agent != null && _agent.isActiveAndEnabled && _agent.isOnNavMesh;
+		if (config.FromSpawningPoint)
+		{
+			_startPosition = startPosition;
+		}
+		else
+		{
+			_startPosition = config.CustomCenter;
+		}
+		_roamingSpeed = config.Speed;
+		_roamingDistance = config.Radius;
+	}
+
+	public override void OnUpdate()
+	{
+
+	}
+
+	public override void OnStateEnter()
+	{
+		if (_isActiveAgent)
+		{
+			_roamingTargetPosition = GetRoamingPositionAroundPosition(_startPosition);
+			_agent.speed = _roamingSpeed;
+			_agent.isStopped = false;
+			_agent.SetDestination(_roamingTargetPosition);
+		}
+	}
+
+	public override void OnStateExit()
+	{
+
+	}
+
+	// Compute a random target position around the starting position.
+	private Vector3 GetRoamingPositionAroundPosition(Vector3 position)
+	{
+		return position + new Vector3(Random.Range(-1, 1), 0.0f, Random.Range(-1, 1)).normalized * Random.Range(_roamingDistance / 2, _roamingDistance);
+	}
+}
