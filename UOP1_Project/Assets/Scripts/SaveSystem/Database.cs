b14f16dc7f48b4741f27d59f610cf4df056057f3
Bronson Zgeb <bronson.zgeb@gmail.com>
2021-02-16 19:29:14 +0000

    [Bot] Automated dotnet-format update

--- UOP1_Project/Assets/Scripts/SaveSystem/Database.cs
+++ UOP1_Project/Assets/Scripts/SaveSystem/Database.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 
 public abstract class Database<T> : ScriptableObject where T : IDesc
 {
-    [SerializeField] protected List<T> _descs = new List<T>();
+	[SerializeField] protected List<T> _descs = new List<T>();
 
-    public virtual object FindByUuid(string uuid)
-    {
-        foreach (var desc in _descs)
-        {
-            if (desc.DescId.uuid == uuid)
-            {
-                return desc;
-            }
-        }
+	public virtual object FindByUuid(string uuid)
+	{
+		foreach (var desc in _descs)
+		{
+			if (desc.DescId.uuid == uuid)
+			{
+				return desc;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 }
--- UOP1_Project/Assets/Scripts/SaveSystem/Uuid.cs
+++ UOP1_Project/Assets/Scripts/SaveSystem/Uuid.cs
@@ -1,11 +1,11 @@
 [System.Serializable]
 public class Uuid
 {
-    public string uuid;
+	public string uuid;
 
-    public static Uuid Generate()
-    {
-        Uuid newUuid = new Uuid {uuid = System.Guid.NewGuid().ToString()};
-        return newUuid;
-    }
+	public static Uuid Generate()
+	{
+		Uuid newUuid = new Uuid { uuid = System.Guid.NewGuid().ToString() };
+		return newUuid;
+	}
 }
