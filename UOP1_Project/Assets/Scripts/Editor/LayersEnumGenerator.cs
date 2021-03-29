ef7f675b446b538bc2079865cc0a78263e6ebdfb
Andrew Menino-Barlow <andrew.barlow@gmail.com>
2021-03-29 13:02:12 +0100

    Adding a menu item that automatically generates a Layer and LayerMasks enums so we can use their defined values for layer comparison in the scripts.
    The generator is pre-configured to generate a file called "Layers.cs". It contains a pair of enums; Layer and LayerMasks. These reside in the "UOP1" namespace. These enums contains the layer ID's in the project and their respective layer masks. The LayerMasks enum has the Flags attribute applied to allow you to correctly use these LayerMasks to check against multiple layers at once, such as in Physics.Raycast.
    
    There are protections in the generator to stop accidental overwriting of already existing files in the project should the configuration values change.
    
    The generated Layer file should be committed to the repository (and is included in this commit) to save others from regenerating it. The generated file also contains instructions on how to use it and how to regenerate the file should a new layer be added to the project.
    
    Finally, I included a handful of extension methods for UnityEngine.LayerMask to make it easy to check if a layer is contained within a UnityEngine.LayerMask.

---
+++ UOP1_Project/Assets/Scripts/Editor/LayersEnumGenerator.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Reflection;
+using Microsoft.CSharp;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using static System.String;
+
+namespace UOP1.EditorTools
+{
+	/// <summary>
+	/// Generates a pair of <see langword="enum"/>s which contain the layer IDs and corresponding layer masks.
+	/// </summary>
+	internal static class LayersEnumGenerator
+	{
+		#region Configuration
+
+		/// <summary>
+		/// The <see cref="MenuItem" /> name.
+		/// </summary>
+		private const string LAYERS_MENU_ITEM = "ChopChop/Code Generation/Layers";
+
+		/// <summary>
+		/// The name of the class to enum.
+		/// </summary>
+		private const string LAYERS_ENUM_NAME = "Layer";
+
+		/// <summary>
+		/// The name of the Layer Masks enum to create.
+		/// </summary>
+		private const string LAYERMASKS_ENUM_NAME = "LayerMasks";
+
+		/// <summary>
+		/// Optional namespace to put the enums in. Can be '<see langword="null" />' or empty..
+		/// </summary>
+		private const string LAYERS_ENUM_NAMESPACE = "UOP1";
+
+		/// <summary>
+		/// The path relative to the project's asset folder.
+		/// </summary>
+		private const string LAYERS_ENUM_PATH = "Scripts/Layers.cs";
+
+		#endregion
+
+		/// <summary>
+		/// The absolute path to the file containing the enums.
+		/// </summary>
+		private static readonly string LayersFilePath = $"{Application.dataPath}/{LAYERS_ENUM_PATH}";
+
+		/// <summary>
+		/// Validates if we can generate a new layers file.
+		/// </summary>
+		/// <remarks>
+		/// Can only generate a new file if the following conditions are met:
+		///     - <see cref="LAYERS_ENUM_NAME" /> is not null or a whitespace.
+		///     - <see cref="LAYERS_ENUM_PATH" /> is not null or a whitespace.
+		///     - <see cref="LayersFilePath" /> doesn't already exist.
+		/// </remarks>
+		/// <remarks>
+		/// These are protections against accidentally overwriting another file if the configuration values are changed.
+		/// </remarks>
+		/// <returns>
+		/// <see langword="true" /> if all conditions are met.
+		/// </returns>
+		[MenuItem(LAYERS_MENU_ITEM, true)]
+		private static bool CanGenerate()
+		{
+			if (IsNullOrWhiteSpace(LAYERS_ENUM_NAME)) return false;
+			if (IsNullOrWhiteSpace(LAYERMASKS_ENUM_NAME)) return false;
+			if (IsNullOrWhiteSpace(LAYERS_ENUM_PATH)) return false;
+			if (File.Exists(LayersFilePath)) return false;
+
+			return true;
+		}
+
+		/// <summary>
+		/// Generates a new Layers class file.
+		/// </summary>
+		[MenuItem(LAYERS_MENU_ITEM)]
+		private static void Generate()
+		{
+			// Start with a compileUnit to create our code and give it an optional namespace.
+			CodeCompileUnit compileUnit = new CodeCompileUnit();
+			CodeNamespace codeNamespace = new CodeNamespace(LAYERS_ENUM_NAMESPACE);
+			compileUnit.Namespaces.Add(codeNamespace);
+			codeNamespace.Imports.Add(new CodeNamespaceImport(nameof(System)));
+
+			// Declare a pair of enum types that are public.
+			CodeTypeDeclaration layersEnum = new CodeTypeDeclaration(LAYERS_ENUM_NAME)
+			{
+				IsEnum = true,
+				TypeAttributes = TypeAttributes.Public
+			};
+
+			CodeTypeDeclaration layerMasksEnum = new CodeTypeDeclaration(LAYERMASKS_ENUM_NAME)
+			{
+				IsEnum = true,
+				TypeAttributes = TypeAttributes.Public
+			};
+
+			// Put the Flags attribute on the LayerMasks enum to allow us to check multiple layers at once.
+			layerMasksEnum.CustomAttributes.Add(new CodeAttributeDeclaration(nameof(FlagsAttribute)));
+
+			// Add some comments so the class describes it's intended usage.
+			AddCommentsToLayerEnum(layersEnum);
+			AddCommentsToLayerMasksEnum(layerMasksEnum);
+
+			// Create members in both of the enums for each layer in the project.
+			CreateLayerMembers(layersEnum, layerMasksEnum);
+
+			// Add the type declarations to the namespace.
+			codeNamespace.Types.Add(layersEnum);
+			codeNamespace.Types.Add(layerMasksEnum);
+
+			// With a StringWriter and a CSharpCodeProvider; generate the code.
+			using StringWriter stringWriter = new StringWriter();
+			using CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+
+			codeProvider.GenerateCodeFromCompileUnit(compileUnit, stringWriter, new CodeGeneratorOptions
+			{
+				BracingStyle = "C",
+				BlankLinesBetweenMembers = false
+			});
+
+			// Create the asset path if it doesn't already exist.
+			CreateAssetPathIfNotExists(LayersFilePath);
+
+			// Write the code to the file system and refresh the AssetDatabase.
+			File.WriteAllText(LayersFilePath, stringWriter.ToString());
+			AssetDatabase.Refresh();
+		}
+
+		/// <summary>
+		/// Adds a verbose comment on how to use the Layer enum.
+		/// </summary>
+		/// <param name="typeDeclaration">The <see cref="CodeTypeDeclaration"/> to add the comment to.</param>
+		private static void AddCommentsToLayerEnum(CodeTypeDeclaration typeDeclaration)
+		{
+			CodeCommentStatement commentStatement = new CodeCommentStatement(
+				"<summary>\n Use this enum in place of layer names in code / scripts.\n </summary>" +
+				"\n <example>\n <code>\n if (other.gameObject.layer == Layer.Characters) {\n     Destroy(other.gameObject);" +
+				"\n }\n </code>\n </example>\n <remarks>\n <b>Important</b>: To regenerate this class after adding or removing " +
+				$"layers to the project; delete\n \"{LAYERS_ENUM_PATH}\" via the \"Project Window\" and use the " +
+				$"\"{LAYERS_MENU_ITEM}\" menu to regenerate it.\n </remarks>",
+				true);
+
+			typeDeclaration.Comments.Add(commentStatement);
+		}
+
+		/// <summary>
+		/// Adds a verbose comment on how to use the LayerMask enum.
+		/// </summary>
+		/// <param name="typeDeclaration">The <see cref="CodeTypeDeclaration"/> to add the comment to.</param>
+		private static void AddCommentsToLayerMasksEnum(CodeTypeDeclaration typeDeclaration)
+		{
+			CodeCommentStatement commentStatement = new CodeCommentStatement(
+				"<summary>\n Use this enum in place of layer mask values in code / scripts.\n </summary>" +
+				"\n <example>\n <code>\n if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), " +
+				"out RaycastHit hit, Mathf.Infinity, (int) (LayerMasks.Characters | LayerMasks.Water)) {\n     " +
+				"Debug.Log(\"Did Hit\");\n }\n </code>\n </example>\n <remarks>\n <b>Important</b>: To regenerate this " +
+				$"class after adding or removing layers to the project; delete\n \"{LAYERS_ENUM_PATH}\" via the \"Project " +
+				$"Window\" and use the \"{LAYERS_MENU_ITEM}\" menu to regenerate it.\n </remarks>",
+				true);
+
+			typeDeclaration.Comments.Add(commentStatement);
+		}
+
+		/// <summary>
+		/// Creates members for each layers in the project and adds them to the <paramref name="layersEnum"/> and <paramref name="layerMasksEnum"/>.
+		/// </summary>
+		/// <param name="layersEnum">The <see cref="CodeTypeDeclaration"/> to add the layer ID's to.</param>
+		/// <param name="layerMasksEnum">The <see cref="CodeTypeDeclaration"/> to add the layer masks to.</param>
+		private static void CreateLayerMembers(CodeTypeDeclaration layersEnum, CodeTypeDeclaration layerMasksEnum)
+		{
+			foreach (string layer in InternalEditorUtility.layers)
+			{
+				string layerName = layer.Replace(" ", Empty);
+				int layerValue = LayerMask.NameToLayer(layer);
+
+				CodeMemberField field = new CodeMemberField(LAYERS_ENUM_NAME, layerName)
+				{
+					InitExpression = new CodePrimitiveExpression(layerValue)
+				};
+				layersEnum.Members.Add(field);
+
+				field = new CodeMemberField(LAYERMASKS_ENUM_NAME, layerName)
+				{
+					InitExpression = new CodePrimitiveExpression(1 << layerValue)
+				};
+				layerMasksEnum.Members.Add(field);
+			}
+		}
+
+		/// <summary>
+		/// Creates the path for the file asset.
+		/// </summary>
+		/// <param name="path">The path to use to create the file asset.</param>
+		private static void CreateAssetPathIfNotExists(string path)
+		{
+			path = path.Remove(path.LastIndexOf('/'));
+
+			if (!Directory.Exists(path))
+				Directory.CreateDirectory(path);
+		}
+	}
+}
---
+++ UOP1_Project/Assets/Scripts/Extensions/UnityEngine/LayerMaskExtensions.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace UOP1.Extensions.UnityEngine
+{
+	/// <summary>
+	/// Extensions for <see cref="LayerMask"/>.
+	/// </summary>
+	public static class LayerMaskExtensions
+	{
+		/// <summary>
+		/// Check if a layer int exists in <see cref="LayerMask"/>.
+		/// </summary>
+		/// <param name="layerMask">The <see cref="LayerMask"/> to check against.</param>
+		/// <param name="layer">The layer int to check.</param>
+		/// <returns>True if <paramref name="layer"/> is in <paramref name="layerMask"/>.</returns>
+		public static bool Contains(this LayerMask layerMask, int layer)
+		{
+			return layerMask == (layerMask | (1 << layer));
+		}
+
+		/// <summary>
+		/// Check if a <see cref="Layer"/> exists in the <see cref="LayerMask"/>.
+		/// </summary>
+		/// <param name="layerMask">The <see cref="LayerMask"/> to check against.</param>
+		/// <param name="layer">The <see cref="Layer"/> to check.</param>
+		/// <returns>True if <paramref name="layer"/> is in <paramref name="layerMask"/>.</returns>
+		public static bool Contains(this LayerMask layerMask, Layer layer)
+		{
+			return layerMask.Contains((int)layer);
+		}
+
+		/// <summary>
+		/// Check if a <see cref="GameObject"/>'s layer exists in the <see cref="LayerMask"/>.
+		/// </summary>
+		/// <param name="layerMask">The <see cref="LayerMask"/> to check against.</param>
+		/// <param name="gameObject">The <see cref="GameObject"/>'s layer to check.</param>
+		/// <returns>True if <paramref name="gameObject"/>.layer is in <paramref name="layerMask"/>.</returns>
+		public static bool Contains(this LayerMask layerMask, GameObject gameObject)
+		{
+			Assert.IsNotNull(gameObject);
+			return layerMask.Contains(gameObject.layer);
+		}
+	}
+}
