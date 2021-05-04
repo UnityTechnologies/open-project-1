b8d00390b70f4f7dc26de803c53f4c258e79d3f1
Amel <amel.negra@unity3d.com>
2021-05-04 10:29:31 +0200

    Townsfolk script to control when talking by default or not

---
+++ UOP1_Project/Assets/Scripts/Characters/Townsfolk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Townsfolk : MonoBehaviour
+{
+	public bool isTalking;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
