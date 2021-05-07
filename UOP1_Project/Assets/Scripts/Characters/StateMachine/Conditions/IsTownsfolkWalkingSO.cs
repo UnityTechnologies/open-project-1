43f8e866e722eb6c42024207ffd2986d63a63a25
Amel Negra <amel.negra@unity3d.com>
2021-05-07 17:28:42 +0200

    Added townsfolk walking condition

---
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Conditions/IsTownsfolkWalkingSO.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(menuName = "State Machines/Conditions/Is Townsfolk Walking")]
+public class IsTownsfolkWalkingSO : StateConditionSO<IsTownsfolkWalkingCondition> { }
+
+public class IsTownsfolkWalkingCondition : Condition
+{
+	//Component references
+	private Townsfolk _townsfolkScript;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_townsfolkScript = stateMachine.GetComponent<Townsfolk>();
+	}
+	
+	protected override bool Statement()
+	{
+
+		if (_townsfolkScript.townsfolkInitialState == InitialState.Walk)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+}
