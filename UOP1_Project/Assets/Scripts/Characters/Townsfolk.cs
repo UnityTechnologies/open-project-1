10a5d31d10e3910c13e6e9d3cb25b5fd001bfa57
Amel Negra <amel.negra@unity3d.com>
2021-05-14 17:24:13 +0200

    Townsfolk to stop talking when protagonist talks to others in conversation

--- UOP1_Project/Assets/Scripts/Characters/Townsfolk.cs
+++ UOP1_Project/Assets/Scripts/Characters/Townsfolk.cs
@@ -3,20 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum InitialState { Idle = 0, Walk, Talk };
+public enum TownsfolkState { Idle = 0, Walk, Talk };
 
 
 public class Townsfolk : MonoBehaviour
 {
-	public InitialState InitialState; //This is checked by conditions in the StateMachine
+	public TownsfolkState townsfolkState; //This is checked by conditions in the StateMachine
+	public GameObject[] talkingTo;
 
-	void Start()
-    {
-        
-    }
+	private DialogueDataChannelSO _startDialogueEvent = default;
+	private VoidEventChannelSO _endDialogueEvent = default;
 
-    void Update()
+	private void OnEnable()
+	{
+		if (_startDialogueEvent != null)
+			_startDialogueEvent.OnEventRaised += StopTalking;
+
+		if (_endDialogueEvent != null)
+			_endDialogueEvent.OnEventRaised += ResumeTalking;
+	}
+
+
+	private void StopTalking(DialogueDataSO dialogue)
     {
-        
-    }
+		for (int i = 0; i < talkingTo.Length; ++i)
+		{
+			if (talkingTo[i].GetComponent<StepController>().IsInDialogue)
+			{
+				townsfolkState = TownsfolkState.Idle;
+				return;
+			}
+		}
+	}
+
+	private void ResumeTalking()
+	{
+		for (int i = 0; i < talkingTo.Length; ++i)
+		{
+			if (talkingTo[i].GetComponent<StepController>().IsInDialogue)
+				return;
+		}
+		townsfolkState = TownsfolkState.Talk;
+
+	}
 }
