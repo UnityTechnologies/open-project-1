1c759c66d9ed0e45da4f732e0e6e20b3c74eaf36
Amel Negra <amel.negra@unity3d.com>
2021-05-17 15:50:47 +0200

    edited townsfolk script to move methods to step controller

--- UOP1_Project/Assets/Scripts/Characters/Townsfolk.cs
+++ UOP1_Project/Assets/Scripts/Characters/Townsfolk.cs
@@ -2,48 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public enum TownsfolkState { Idle = 0, Walk, Talk };
-
 
 public class Townsfolk : MonoBehaviour
 {
 	public TownsfolkState townsfolkState; //This is checked by conditions in the StateMachine
 	public GameObject[] talkingTo;
-
-	private DialogueDataChannelSO _startDialogueEvent = default;
-	private VoidEventChannelSO _endDialogueEvent = default;
-
-	private void OnEnable()
-	{
-		if (_startDialogueEvent != null)
-			_startDialogueEvent.OnEventRaised += StopTalking;
-
-		if (_endDialogueEvent != null)
-			_endDialogueEvent.OnEventRaised += ResumeTalking;
-	}
-
-
-	private void StopTalking(DialogueDataSO dialogue)
-    {
-		for (int i = 0; i < talkingTo.Length; ++i)
-		{
-			if (talkingTo[i].GetComponent<StepController>().IsInDialogue)
-			{
-				townsfolkState = TownsfolkState.Idle;
-				return;
-			}
-		}
-	}
-
-	private void ResumeTalking()
-	{
-		for (int i = 0; i < talkingTo.Length; ++i)
-		{
-			if (talkingTo[i].GetComponent<StepController>().IsInDialogue)
-				return;
-		}
-		townsfolkState = TownsfolkState.Talk;
-
-	}
 }
+
