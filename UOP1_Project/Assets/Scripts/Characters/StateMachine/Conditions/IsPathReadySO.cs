93864eee3536698adb386c9300e773d1d7b18fc0
Amel Negra <amel.negra@unity3d.com>
2021-05-19 14:40:49 +0200

    Added is path ready condition

---
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Conditions/IsPathReadySO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+
+[CreateAssetMenu(fileName = "IsPathReady", menuName = "State Machines/Conditions/Is Path Ready")]
+public class IsPathReadySO : StateConditionSO
+{
+	protected override Condition CreateCondition() => new IsPathReady();
+}
+
+public class IsPathReady : Condition
+{
+	private NavMeshAgent _agent;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_agent = stateMachine.gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	protected override bool Statement()
+	{
+		return _agent.pathPending;
+	}
+}
