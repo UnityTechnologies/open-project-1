5b2fe14312f835c88c1505f9fa92066a3ddff618
uChema <59874519+uChema@users.noreply.github.com>
2021-06-04 12:12:07 +0200

    Add a StepToDialogue function to the StepSO. Fix preview display in the Quest tool

---
+++ UOP1_Project/Assets/Scripts/Quests/Editor/DialoguePreview.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.UIElements;
+
+public class DialoguePreview : MonoBehaviour
+{
+	private VisualElement CreateDialoguePreviewWithBranching(DialogueDataSO dialogueDataSO)
+	{
+		VisualElement dialoguePreviewVE = new VisualElement();
+
+		DialogueDataSO currentDialogue = dialogueDataSO;
+
+		foreach (LocalizedString localizedString in currentDialogue.DialogueLines)
+		{
+			Label dialogueLine = new Label();
+			dialogueLine.text = localizedString.GetLocalizedStringImmediateSafe();
+			dialoguePreviewVE.Add(dialogueLine);
+		}
+		if (currentDialogue.Choices != null)
+		{
+			foreach (Choice choice in currentDialogue.Choices)
+			{
+				Button dialogueButton = new Button();
+				dialogueButton.text = choice.Response.GetLocalizedStringImmediateSafe();
+				dialoguePreviewVE.Add(dialogueButton);
+				dialoguePreviewVE.Add(CreateDialoguePreviewWithBranching(choice.NextDialogue));
+			}
+		}
+
+
+		return dialoguePreviewVE;
+	}
+
+}
