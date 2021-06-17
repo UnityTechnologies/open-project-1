e08ed5c259d00e87fbeeff7d1a6b1ba8f62294be
Amel <amel.negra@unity3d.com>
2021-06-17 16:48:20 +0200

    Added getting hit and faint sounds for protagonist

--- UOP1_Project/Assets/Scripts/Characters/ProtagonistAudio.cs
+++ UOP1_Project/Assets/Scripts/Characters/ProtagonistAudio.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ProtagonistAudio : CharacterAudio
 {
-	[SerializeField] private AudioCueSO caneSwing, objectPickup, footstep;
+	[SerializeField] private AudioCueSO caneSwing, objectPickup, footstep, getHit, die;
 
 	public void PlayFootstep() => _sfxEventChannel.RaisePlayEvent(footstep, _audioConfig, transform.position);
 	public void PlayCaneSwing() => _sfxEventChannel.RaisePlayEvent(caneSwing, _audioConfig, transform.position);
 	public void PlayObjectPickup() => _sfxEventChannel.RaisePlayEvent(objectPickup, _audioConfig, transform.position);
+	public void PlayGetHit() => _sfxEventChannel.RaisePlayEvent(getHit, _audioConfig, transform.position);
+	public void PlayDie() => _sfxEventChannel.RaisePlayEvent(die, _audioConfig, transform.position);
+
 }
