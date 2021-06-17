72cc017dc2e2fe73907e422eff8b14cfe69016dc
Amel <amel.negra@unity3d.com>
2021-06-17 16:46:57 +0200

    Added sing short and long to NPC Audio script

--- UOP1_Project/Assets/NPCAudio.cs
+++ UOP1_Project/Assets/NPCAudio.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class NPCAudio : CharacterAudio
 {
-	[SerializeField] private AudioCueSO idleSound, talk, footstep;
+	[SerializeField] private AudioCueSO singShort, singLong, talk, footstep;
 
 	//when we have the ground detector script, we should check the type to know which footstep sound to play
 	public void PlayFootstep() => _sfxEventChannel.RaisePlayEvent(footstep, _audioConfig, transform.position);
 	public void PlayTalk() => _sfxEventChannel.RaisePlayEvent(talk, _audioConfig, transform.position);
 	//Only bard hare will use the Idle since he sings at that time
-	public void PlayIdle() => _sfxEventChannel.RaisePlayEvent(idleSound, _audioConfig, transform.position);
+	public void PlaySingShort() => _sfxEventChannel.RaisePlayEvent(singShort, _audioConfig, transform.position);
+	public void PlaySingLong() => _sfxEventChannel.RaisePlayEvent(singLong, _audioConfig, transform.position);
 
 }
