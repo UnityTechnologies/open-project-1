c82753d92992f1a904787242ee2ce88b4d17cc6c
Ciro Continisio <ciro@unity3d.com>
2021-06-19 14:37:00 +0200

    PigChef tweaks
    Added jump and land sfx
    Jump animation now broken in 3 states (liftoff, in air, land)
    Added extra late transition between cane swings, so the character is never waiting idle too long when the attack button is pressed

--- UOP1_Project/Assets/Scripts/Characters/ProtagonistAudio.cs
+++ UOP1_Project/Assets/Scripts/Characters/ProtagonistAudio.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ProtagonistAudio : CharacterAudio
 {
-	[SerializeField] private AudioCueSO caneSwing, objectPickup, footstep, getHit, die;
+	[SerializeField] private AudioCueSO caneSwing, liftoff, land, objectPickup, footstep, getHit, die;
 
 	public void PlayFootstep() => _sfxEventChannel.RaisePlayEvent(footstep, _audioConfig, transform.position);
+	public void PlayJumpLiftoff() => _sfxEventChannel.RaisePlayEvent(liftoff, _audioConfig, transform.position);
+	public void PlayJumpLand() => _sfxEventChannel.RaisePlayEvent(land, _audioConfig, transform.position);
 	public void PlayCaneSwing() => _sfxEventChannel.RaisePlayEvent(caneSwing, _audioConfig, transform.position);
 	public void PlayObjectPickup() => _sfxEventChannel.RaisePlayEvent(objectPickup, _audioConfig, transform.position);
 	public void PlayGetHit() => _sfxEventChannel.RaisePlayEvent(getHit, _audioConfig, transform.position);
