b08f6859ddc489b355833afb9ee6a5c4e12dab00
Ciro Continisio <ciro@unity3d.com>
2021-06-24 02:09:28 +0200

    Location entrance camera
    We can now configure a custom camera shot for each location entrance

--- UOP1_Project/Assets/Scripts/SceneManagement/LocationEntrance.cs
+++ UOP1_Project/Assets/Scripts/SceneManagement/LocationEntrance.cs
@@ -1,10 +1,39 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LocationEntrance : MonoBehaviour
 {
 	[SerializeField] private PathSO _entrancePath;
+	[SerializeField] private PathStorageSO _pathStorage = default; //This is where the last path taken has been stored
+	[SerializeField] private CinemachineVirtualCamera entranceShot;
 
+	[Header("Lisenting on")]
+	[SerializeField] private VoidEventChannelSO _onSceneReady;
 	public PathSO EntrancePath => _entrancePath;
+
+	private void Awake()
+	{
+		if(_pathStorage.lastPathTaken == _entrancePath)
+		{
+			entranceShot.Priority = 100;
+			_onSceneReady.OnEventRaised += PlanTransition;
+		}
+	}
+
+	private void PlanTransition()
+	{
+		StartCoroutine(TransitionToGameCamera());
+	}
+
+	private IEnumerator TransitionToGameCamera()
+	{
+
+		yield return new WaitForSeconds(.1f);
+
+		entranceShot.Priority = -1;
+		_onSceneReady.OnEventRaised -= PlanTransition;
+	}
 }
