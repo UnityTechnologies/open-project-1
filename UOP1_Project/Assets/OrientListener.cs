22c3c12e3b2def55187ca905a6b4c58ae632fbda
pointfivetee <25410392+pointfivetee@users.noreply.github.com>
2021-06-30 12:31:37 -0400

    Use TransformAnchor to compute listener orientation

--- UOP1_Project/Assets/OrientListener.cs
+++ UOP1_Project/Assets/OrientListener.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 // Orient the listener to point in the same direction as the camera.
 public class OrientListener : MonoBehaviour
 {
+    // Reference to the camera transform determine listener orientation
+	[SerializeField]
+	private TransformAnchor _cameraTransform;
+
     void LateUpdate()
     {
-        transform.forward = Camera.main.transform.forward;
+        transform.forward = _cameraTransform.Transform.forward;
     }
 }
