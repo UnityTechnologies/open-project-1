6cdfe30f0921f5f6df42197d60bc6350f7d2cd2d
Amel Negra <amel.negra@unity3d.com>
2021-07-07 11:36:50 +0200

    Cutscene can be triggered by specific cinematic events (+ kept collider trigger for test purpose)

--- UOP1_Project/Assets/Scripts/Cutscenes/CutsceneTrigger.cs
+++ UOP1_Project/Assets/Scripts/Cutscenes/CutsceneTrigger.cs
@@ -7,19 +7,45 @@ using UnityEngine.Playables;
 
 public class CutsceneTrigger : MonoBehaviour
 {
-	[SerializeField] private PlayableDirector _playableDirector = default;
+	private PlayableDirector _playableDirector = default;
 	[SerializeField] private bool _playOnStart = default;
 	[SerializeField] private bool _playOnce = default;
 
+	[Header("Listening to channels")]
+	[SerializeField] private VoidEventChannelSO _playSpeceficCutscene = default;
+
+	[Header("Broadcasting on channels")]
 	[SerializeField] private PlayableDirectorChannelSO _playCutsceneEvent = default;
+
 
 	private void Start()
 	{
+		_playableDirector = GetComponent<PlayableDirector>();
 		if (_playOnStart)
 			if (_playCutsceneEvent != null)
 				_playCutsceneEvent.RaiseEvent(_playableDirector);
 	}
 
+	private void OnEnable()
+	{
+		_playSpeceficCutscene.OnEventRaised += PlaySpeceficCutscene;
+	}
+	private void OnDisable()
+	{
+		_playSpeceficCutscene.OnEventRaised -= PlaySpeceficCutscene;
+	}
+
+	void PlaySpeceficCutscene()
+	{
+		if (_playCutsceneEvent != null)
+			_playCutsceneEvent.RaiseEvent(_playableDirector);
+
+		if (_playOnce)
+			Destroy(this);
+	}
+
+	//THIS WILL BE REMOVED LATER WHEN WE HAVE ALL EVENTS SET UP, NOW WE ONLY NEED IT TO TEST CUTSCENE WITH TRIGGER
+	//Remember to remove collider componenet when we remove this
 	private void OnTriggerEnter(Collider other)
 	{
 		if (_playCutsceneEvent != null)
