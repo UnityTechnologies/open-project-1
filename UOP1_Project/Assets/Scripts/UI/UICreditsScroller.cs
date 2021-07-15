8687fad4f24377b2f7f181319615ad3ae896e326
GoodBoySK <87279821+GoodBoySK@users.noreply.github.com>
2021-07-15 23:50:57 +0200

    WIP: Faster and backward scrolling feature added
    I added feature to scroll faster and backwards.
    
    I am not sure about implementing Input system because i little lost here.

--- UOP1_Project/Assets/Scripts/UI/UICreditsScroller.cs
+++ UOP1_Project/Assets/Scripts/UI/UICreditsScroller.cs
@@ -3,19 +3,22 @@ using TMPro;
 
 public class UICreditsScroller : MonoBehaviour
 {
+	[SerializeField] private InputReader inputReader;
 
 
 	public TextMeshProUGUI textCredits;
 	public RectTransform mask;
-	[SerializeField,Tooltip("Set speed of a scrolling effect")]private float speed = 5f;
-
+	[SerializeField,Tooltip("Set speed of a scrolling effect")]private float _speed = 5f;
+	[SerializeField]private float speed = 5f;
 
 	private float expectedFinishingPoint;
 
 	// Start is called before the first frame update
     void Start()
     {
+		speed = _speed;
 		Invoke("offsetStart",0.01f);
+		inputReader.moveEvent += OnMove;
 	}
 
     // Update is called once per frame
@@ -25,9 +28,27 @@ public class UICreditsScroller : MonoBehaviour
 		{
 			textCredits.rectTransform.position = new Vector2(textCredits.transform.position.x, textCredits.rectTransform.position.y + speed * Time.deltaTime);
 		}
-    }
+	}
 	private void offsetStart() {
+		inputReader.EnableGameplayInput();
 		textCredits.transform.position = new Vector2(textCredits.transform.position.x, -(textCredits.rectTransform.sizeDelta.y / 2));
 		expectedFinishingPoint = textCredits.rectTransform.sizeDelta.y + mask.sizeDelta.y + textCredits.transform.position.y;
+	}
+	private void OnMove(Vector2 direction)
+	{
+		Debug.Log("y: " + direction.y);
+		if (direction.y == 0f)
+		{
+			speed = _speed;
+		}
+		else
+			if (direction.y > 0f)
+			{
+			speed = speed * 2;
+			}
+			else
+			{
+				speed = -_speed;
+			}
 	}
 }
