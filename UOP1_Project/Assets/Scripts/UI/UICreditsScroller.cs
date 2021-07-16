15cc1d6a56343ac36ffdce96c3ffd918e65c6142
GoodBoySK <87279821+GoodBoySK@users.noreply.github.com>
2021-07-16 21:58:44 +0200

    Finish event added, Fixed scrolling code
    I fixed my position mistake type in code.
    i am still not sure about a input system integration i do in my script :)

--- UOP1_Project/Assets/Scripts/UI/UICreditsScroller.cs
+++ UOP1_Project/Assets/Scripts/UI/UICreditsScroller.cs
@@ -6,49 +6,74 @@ public class UICreditsScroller : MonoBehaviour
 	[SerializeField] private InputReader inputReader;
 
 
-	public TextMeshProUGUI textCredits;
+	public RectTransform textCredits;
 	public RectTransform mask;
-	[SerializeField,Tooltip("Set speed of a scrolling effect")]private float _speed = 5f;
-	[SerializeField]private float speed = 5f;
-
+	[SerializeField,Tooltip("Set speed of a scrolling effect")]private float _speed = 5f;//normal scrolling speed
+	[SerializeField]private float speed = 5f;//actual speed of scrolling
+	[SerializeField]private VoidEventChannelSO creditsScrollEndEvent= default;
 	private float expectedFinishingPoint;
-
+	[SerializeField] private UICredits _uiCredits;
+	
+	public bool scrolAgain = false;
 	// Start is called before the first frame update
     void Start()
     {
 		speed = _speed;
-		Invoke("offsetStart",0.01f);
+		Invoke("offsetStart",0.01f);//This offset is needed to get true informations about rectangle and his mask
 		inputReader.moveEvent += OnMove;
 	}
-
-    // Update is called once per frame
-    void Update()
+	private void OnDestroy()
+	{
+		inputReader.moveEvent -= OnMove;
+	}
+	// Update is called once per frame
+	void Update()
     {
-		if (textCredits.rectTransform.position.y < expectedFinishingPoint)
+
+		//This make scrolling effect
+		if (textCredits.anchoredPosition.y < expectedFinishingPoint)
 		{
-			textCredits.rectTransform.position = new Vector2(textCredits.transform.position.x, textCredits.rectTransform.position.y + speed * Time.deltaTime);
+			textCredits.anchoredPosition = new Vector2(textCredits.anchoredPosition.x, textCredits.anchoredPosition.y + speed * Time.deltaTime);
+		}
+		else if(expectedFinishingPoint != 0)//this happend when scrolling reach to end
+		{
+			ScrollingEnd();
 		}
 	}
 	private void offsetStart() {
 		inputReader.EnableGameplayInput();
-		textCredits.transform.position = new Vector2(textCredits.transform.position.x, -(textCredits.rectTransform.sizeDelta.y / 2));
-		expectedFinishingPoint = textCredits.rectTransform.sizeDelta.y + mask.sizeDelta.y + textCredits.transform.position.y;
+		expectedFinishingPoint = (textCredits.rect.height + mask.rect.height) / 2;
+		
+		textCredits.anchoredPosition = new Vector2(textCredits.anchoredPosition.x, -((textCredits.rect.height + mask.rect.height )/ 2 ) );
+
 	}
 	private void OnMove(Vector2 direction)
 	{
-		Debug.Log("y: " + direction.y);
 		if (direction.y == 0f)
 		{
 			speed = _speed;
 		}
-		else
-			if (direction.y > 0f)
-			{
+		else if (direction.y > 0f)
+		{
 			speed = speed * 2;
-			}
-			else
-			{
-				speed = -_speed;
-			}
+		}
+		else
+		{
+			speed = -_speed;
+		}
+	}
+	private void ScrollingEnd()
+	{
+		creditsScrollEndEvent.RaiseEvent();
+		if (scrolAgain == true)//reset postion of an element
+		{
+			textCredits.anchoredPosition = new Vector2(textCredits.anchoredPosition.x, -((textCredits.rect.height + mask.rect.height) / 2));
+
+		}
+		else
+		{
+			//close credits
+			_uiCredits.CloseCreditsScreen();
+		}
 	}
 }
