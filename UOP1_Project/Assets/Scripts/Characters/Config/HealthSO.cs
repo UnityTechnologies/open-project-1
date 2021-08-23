83f4b606af60e590328c407de01208ea34b20c7f
uChema <59874519+uChema@users.noreply.github.com>
2021-08-23 14:41:00 +0200

    Keep health between scenes

---
+++ UOP1_Project/Assets/Scripts/Characters/Config/HealthSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayersHealth", menuName = "EntityConfig/Player's Health")]
+public class HealthSO : ScriptableObject
+{
+	[Tooltip("Initial health")]
+	[SerializeField] private int _maxHealth;
+	[Tooltip("current health")]
+	[SerializeField] private int _currentHealth;
+
+	public int MaxHealth { get; set; }
+	public int CurrentHealth { get; set; }
+}
--- UOP1_Project/Assets/Scripts/Characters/Damageable.cs
+++ UOP1_Project/Assets/Scripts/Characters/Damageable.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public class Damageable : MonoBehaviour
 {
 	[SerializeField] private HealthConfigSO _healthConfigSO;
+	[SerializeField] private HealthSO _currentHealthSO;
 	[SerializeField] private GetHitEffectConfigSO _getHitEffectSO;
 	[SerializeField] private Renderer _mainMeshRenderer;
 	[SerializeField] private DroppableRewardConfigSO _droppableRewardSO;
@@ -13,9 +14,9 @@ public class Damageable : MonoBehaviour
 	[SerializeField] private IntEventChannelSO _setHealthBar = default;
 	[SerializeField] private IntEventChannelSO _inflictDamage = default;
 	[SerializeField] private IntEventChannelSO _restoreHealth = default;
+	[SerializeField] private VoidEventChannelSO _deathEvent = default;
 	public DroppableRewardConfigSO DroppableRewardConfig => _droppableRewardSO;
 
-	private int _currentHealth = default;
 
 	public bool GetHit { get; set; }
 	public bool IsDead { get; set; }
@@ -23,21 +24,26 @@ public class Damageable : MonoBehaviour
 	public GetHitEffectConfigSO GetHitEffectConfig => _getHitEffectSO;
 	public Renderer MainMeshRenderer => _mainMeshRenderer;
 
-	public int CurrentHealth => _currentHealth;
 
 	public UnityAction OnDie;
 
 	private void Awake()
 	{
-		_currentHealth = _healthConfigSO.MaxHealth;
+		if (_currentHealthSO == null)
+		{
+			_currentHealthSO = new HealthSO();
+			_currentHealthSO.MaxHealth = _healthConfigSO.MaxHealth;
+			_currentHealthSO.CurrentHealth = _healthConfigSO.MaxHealth;
+		}
+
 		if (_setHealthBar != null)
-			_setHealthBar.RaiseEvent(_currentHealth);
+			_setHealthBar.RaiseEvent(_currentHealthSO.CurrentHealth);
 
 	}
 
 	public void Kill()
 	{
-		ReceiveAnAttack(_currentHealth);
+		ReceiveAnAttack(_currentHealthSO.CurrentHealth);
 	}
 
 	public void ReceiveAnAttack(int damage)
@@ -46,28 +52,31 @@ public class Damageable : MonoBehaviour
 			return;
 		if (_inflictDamage != null)
 			_inflictDamage.RaiseEvent(damage);
-		_currentHealth -= damage;
+		_currentHealthSO.CurrentHealth -= damage;
+
 		GetHit = true;
-		if (_currentHealth <= 0)
+		if (_currentHealthSO.CurrentHealth <= 0)
 		{
 			IsDead = true;
 			if (OnDie != null)
 				OnDie.Invoke();
+			if (_deathEvent != null)
+				_deathEvent.RaiseEvent();
 		}
 	}
 
 	public void ResetHealth()
 	{
-		_currentHealth = _healthConfigSO.MaxHealth;
+		_currentHealthSO.CurrentHealth = _healthConfigSO.MaxHealth;
 		if (_setHealthBar != null)
-			_setHealthBar.RaiseEvent(_currentHealth);
+			_setHealthBar.RaiseEvent(_currentHealthSO.CurrentHealth);
 		IsDead = false;
 	}
 	public void restoreHealth(int healthToAdd)
 	{
 		if (IsDead)
 			return;
-		_currentHealth += healthToAdd;
+		_currentHealthSO.CurrentHealth += healthToAdd;
 		if (_restoreHealth != null)
 			_restoreHealth.RaiseEvent(healthToAdd);
 	}
